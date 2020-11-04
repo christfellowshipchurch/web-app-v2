1dc7b35dcfd5c727405fefa1cc593316e988603e
Drew Barontini <hello@drewb.io>
2020-11-04 09:12:54 -0500

    Add Apollo

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,22 @@
 import Head from 'next/head';
+import { ApolloProvider } from '@apollo/client';
 
+import { useApollo } from '../config/apolloClient';
 import { ThemeProvider } from '../ui-kit';
 
 function App({ Component, pageProps }) {
+  const apolloClient = useApollo(pageProps.initialApolloState);
+
   return (
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <ThemeProvider>
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <ApolloProvider client={apolloClient}>
+        <ThemeProvider>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </ApolloProvider>
     </>
   );
 }
