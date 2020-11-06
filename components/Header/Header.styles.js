aecf51e5fb3b0a8d46979bdd4b0d60fa71ac723f
Drew Barontini <hello@drewb.io>
2020-11-06 12:26:23 -0500

    Add box-shadow to Header

--- components/Header/Header.styles.js
+++ components/Header/Header.styles.js
@@ -5,6 +5,7 @@ import { system } from '../../ui-kit';
 
 const Header = styled.header`
   background-color: ${themeGet('colors.white')};
+  box-shadow: ${themeGet('shadows.base')};
   align-items: center;
   display: grid;
   grid-template-columns: auto 1fr;
