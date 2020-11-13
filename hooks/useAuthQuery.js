5f8251e8b7f7b1407321685d03fd140fd43acf3e
Drew Barontini <hello@drewb.io>
2020-11-13 16:30:48 -0500

    Fix error in useAuthQuery

--- hooks/useAuthQuery.js
+++ hooks/useAuthQuery.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 
 import { useAuth, logout } from '../providers/AuthProvider';
@@ -6,19 +6,28 @@ import { useAuth, logout } from '../providers/AuthProvider';
 const useAuthQuery = (query, options = {}) => {
   const [state, dispatch] = useAuth();
   const { token, authenticated } = state;
-  const { data, error, loading, refetch } = useQuery(query, {
-    skip: !authenticated,
-    fetchPolicy: 'network-only',
-    onError: () => {
-      console.error('Authentication error: logging out...');
-      dispatch(logout());
-    },
-    ...options,
-  });
+  const { data, error, loading, refetch: _refetch } =
+    useQuery(query, {
+      skip: !authenticated,
+      fetchPolicy: 'network-only',
+      onError: () => {
+        console.error('Authentication error: logging out...');
+        dispatch(logout());
+      },
+      ...options,
+    }) || {};
+
+  // There's a bug where the query from `useQuery` is `undefined`
+  // and will throw an error in the `useEffect` down below.
+  // https://github.com/apollographql/react-apollo/issues/3862
+  const refetch = useCallback(() => {
+    if (_refetch) return;
+    setTimeout(() => _refetch(), 0);
+  }, [_refetch]);
 
   useEffect(() => {
     if (authenticated) {
-      refetch().catch(() => console.error('Authentication error'));
+      refetch()?.catch(() => console.error('Authentication error'));
     }
   }, [authenticated, token, refetch]);
 
