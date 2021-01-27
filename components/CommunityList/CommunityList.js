3353a1f965870790c8ab4899c5971426f7f95ced
Jordan Wade <jjwade31@gmail.com>
2021-01-26 22:16:09 -0600

    Filter out community items without images

--- components/CommunityList/CommunityList.js
+++ components/CommunityList/CommunityList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { slugify } from 'utils';
-import { DefaultCard, Box, CardGrid, Loader } from 'ui-kit';
+import { DefaultCard, Box, Loader } from 'ui-kit';
 import { CustomLink } from 'components';
 
 function CommunityList(props = {}) {
@@ -14,22 +14,24 @@ function CommunityList(props = {}) {
 
   return (
     <Box display="flex" flexWrap="wrap" justifyContent="center" margin="-8px">
-      {props.data.map((item, i) => (
-        <CustomLink
-          as="a"
-          key={i}
+      {props.data
+        .filter(item => item?.coverImage?.sources[0]?.uri)
+        .map((item, i) => (
+          <CustomLink
+            as="a"
+            key={i}
             href={item?.url ? item?.url : `/community/${slugify(item?.title)}`}
-          Component={props.component}
-          coverImage={item?.coverImage?.sources[0]?.uri}
-          coverImageOverlay={true}
-          coverImageTitle={item?.title}
-          coverImageDescription={item?.summary}
-          flex="0 0 calc(33.333% - 16px)"
-          margin="8px"
-          height="250px"
-          display="block"
-        />
-      ))}
+            Component={props.component}
+            coverImage={item?.coverImage?.sources[0]?.uri}
+            coverImageOverlay={true}
+            coverImageTitle={item?.title}
+            coverImageDescription={item?.summary}
+            flex="0 0 calc(33.333% - 16px)"
+            margin="8px"
+            height="250px"
+            display="block"
+          />
+        ))}
     </Box>
   );
 }
