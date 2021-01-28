2bb2482128dad49ccf78e7bdb5c830ae812b3b5b
Jordan Wade <jjwade31@gmail.com>
2021-01-28 16:56:49 -0600

    Setup form to submit values

--- components/Modals/ConnectModal/ConnectModal.js
+++ components/Modals/ConnectModal/ConnectModal.js
@@ -1,39 +1,60 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { useForm } from 'hooks';
 
 import { Avatar, Modal, Box, Button, TextInput, Radio } from 'ui-kit';
 
 function ConnectModal(props = {}) {
+  const { values, handleChange, handleSubmit, setValues } = useForm(() => {
+    console.log('Show me Results', values);
+  });
+
+  const defaultMessage = `Hey ${props.leaderName}, I’m interested in joining your Crew group!`;
+
+  useEffect(() => {
+    const initialValue = {
+      message: defaultMessage,
+      preference: 'email',
+    };
+    setValues(initialValue);
+  }, [setValues, defaultMessage]);
+
   return (
     <Modal {...props}>
       {
-        <Box display="flex" flexDirection="column" pl="l" pr="l">
+        <Box
+          as="form"
+          onSubmit={handleSubmit}
+          display="flex"
+          flexDirection="column"
+          pl="l"
+          pr="l"
+        >
           <Box
             display="flex"
             alignItems="center"
             flexDirection="column"
             mb="base"
           >
-            <Avatar
-              src="https://source.unsplash.com/random/200x200"
-              height="100px"
-              width="100px"
-            />
+            <Avatar src={props.leaderAvatar} height="100px" width="100px" />
           </Box>
 
           <Box textAlign="center" as="h2">
-            Connect with Juan
+            {`Connect with ${props.leaderName}`}
           </Box>
           <Box textAlign="center" as="p" mb="l">
             Send a message to let the leader know you’re interested.
           </Box>
+
           <TextInput
-            id="body"
-            multiline
-            placeholder={'Hey Juan, I’m interested in joining your Crew group!'}
+            as="textarea"
+            id="message"
+            label="Message"
+            placeholder={defaultMessage}
             autoFocus
             mb="base"
             height="115px"
+            onChange={handleChange}
           />
           <Box as="p" mb="base">
             The leader will respond with the next steps. How would you like to
@@ -44,8 +65,18 @@ function ConnectModal(props = {}) {
               id="email"
               label="Email"
               containerProps={{ marginRight: 'base' }}
+              name="preference"
+              value="email"
+              defaultChecked={true}
+              onChange={handleChange}
             />
-            <Radio id="phone" label="Phone" />
+            <Radio
+              id="phone"
+              name="preference"
+              label="Phone"
+              value="phone"
+              onChange={handleChange}
+            />
           </Box>
           <Button type="submit" mb="base">
             Send
