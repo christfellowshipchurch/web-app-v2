a93d2107dd43b1ddb5de3922c5aa2e4c46c280c1
Ryan Davidson <ryan@rkdavidson.com>
2021-02-05 14:28:17 -0500

    Button stories updates

--- ui-kit/Button/Button.stories.js
+++ ui-kit/Button/Button.stories.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Box } from 'ui-kit';
 
 import Button from './Button';
 
@@ -9,8 +10,41 @@ export default {
 
 export const Default = () => <Button>Button</Button>;
 
-export const Sizes = () => <Button size="l">Button</Button>;
+export const Sizes = () => (
+  <Box>
+    <Button size="l" mr="s">
+      Large (Default)
+    </Button>
+    <Button size="s">Small</Button>
+  </Box>
+);
 
-export const Variants = () => <Button variant="secondary">Button</Button>;
+export const Variants = () => (
+  <Box>
+    <Button variant="primary" mr="s">
+      Primary
+    </Button>
+    <Button variant="secondary" mr="s">
+      Secondary
+    </Button>
+    <Button variant="tertiary" mr="s">
+      Tertiary
+    </Button>
+  </Box>
+);
+
+export const Rounded = () => (
+  <Box>
+    <Button rounded={true} variant="primary" mr="s">
+      Rounded Primary
+    </Button>
+    <Button rounded={true} variant="secondary" mr="s">
+      Rounded Secondary
+    </Button>
+    <Button rounded={true} variant="tertiary" mr="s">
+      Rounded Tertiary
+    </Button>
+  </Box>
+);
 
 export const Loading = () => <Button status="LOADING">Button</Button>;
