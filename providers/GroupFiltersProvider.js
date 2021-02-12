3821b77fd49a33e0e3af092c76cbaea6ce299b7b
Jordan Wade <jjwade31@gmail.com>
2021-02-12 11:30:02 -0600

    Get group search options from API (#27)
    * Update group filter options hook
    
    * Refactor groups filter options hook and provider
    
    * Return null if no options in FilterField
    
    * Skip subPreferences filter modal when empty
    
    * Fix state hydration bug

--- providers/GroupFiltersProvider.js
+++ providers/GroupFiltersProvider.js
@@ -2,6 +2,7 @@ import React, { createContext, useContext, useEffect, useReducer } from 'react';
 import PropTypes from 'prop-types';
 import useGroupFilterOptions from 'hooks/useGroupFilterOptions';
 import isEmpty from 'lodash/isEmpty';
+import get from 'lodash/get';
 import { useRouter } from 'next/router';
 
 const GroupFiltersProviderStateContext = createContext();
@@ -153,7 +154,6 @@ function reducer(state, action) {
   switch (action.type) {
     case actionTypes.hydrate: {
       return updateAndSerialize({
-        ...initialState,
         hydrated: true,
         values: {
           ...initialState.values,
@@ -214,9 +214,9 @@ function reducer(state, action) {
 
 function GroupFiltersProvider(props = {}) {
   const router = useRouter();
+  const optionsData = useGroupFilterOptions();
 
   const [state, dispatch] = useReducer(reducer, initialState);
-  const optionsData = useGroupFilterOptions();
 
   // Next.js won't have the hydrated state query string available when
   // rendering server-side, so watch for changes to `router.query` and
@@ -230,29 +230,15 @@ function GroupFiltersProvider(props = {}) {
     }
   }, [router?.query, state.hydrated]);
 
-  // To be simplified/removed when we lift group options definitions to API
-  // ✂️ -------------------------------------------------------------------
   useEffect(() => {
-    if (optionsData.campuses?.length) {
-      dispatch(
-        updateOptions({
-          campuses: optionsData.campuses.map(({ name }) => name),
-        })
-      );
-    }
-  }, [optionsData.campuses]);
-
-  useEffect(() => {
-    if (optionsData.preferences?.length && optionsData.subPreferences?.length) {
-      dispatch(
-        updateOptions({
-          preferences: optionsData.preferences.map(({ title }) => title),
-          subPreferences: optionsData.subPreferences.map(({ title }) => title),
-        })
-      );
-    }
-  }, [optionsData.preferences, optionsData.subPreferences]);
-  // ✂️ -------------------------------------------------------------------
+    dispatch(
+      updateOptions({
+        campuses: get(optionsData, 'campusName', []),
+        preferences: get(optionsData, 'preference', []),
+        subPreferences: get(optionsData, 'subPreference', []),
+      })
+    );
+  }, [optionsData]);
 
   return (
     <GroupFiltersProviderStateContext.Provider value={state}>
