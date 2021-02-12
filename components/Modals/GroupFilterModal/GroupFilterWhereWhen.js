d8f80cce44d9aecfa5b85dc1b6d149b6aae5440e
Ryan Davidson <ryan@rkdavidson.com>
2021-02-12 16:34:17 -0500

    WhenWhere filter updates

--- components/Modals/GroupFilterModal/GroupFilterWhereWhen.js
+++ components/Modals/GroupFilterModal/GroupFilterWhereWhen.js
@@ -8,24 +8,23 @@ import {
 } from 'providers/GroupFiltersProvider';
 import { hideModal, useModalDispatch } from 'providers/ModalProvider';
 
-import { Box, Button, Checkbox, Select } from 'ui-kit';
+import { Box, Button, Select } from 'ui-kit';
 
 function GroupFilterWhereWhen(props = {}) {
   const router = useRouter();
   const [filtersState, filtersDispatch] = useGroupFilters();
   const modalDispatch = useModalDispatch();
 
-  const handleChange = event => {
-    event.persist();
+  const handleCampusChange = event => {
     const { name, value } = event.target;
 
-    if (name === 'campuses') {
-      // Campus selection is stored as a multi-select, but the input
-      // is a single-select. Handle it's value specially.
-      filtersDispatch(update({ [name]: value === '' ? [] : [value] }));
-    } else {
-      filtersDispatch(toggleValue({ name, value }));
-    }
+    // Campus selection is stored as a multi-select, but the input
+    // is a single-select. Handle it's value specially.
+    filtersDispatch(update({ [name]: value === '' ? [] : [value] }));
+  };
+
+  const handleDayChange = value => {
+    filtersDispatch(toggleValue({ name: 'days', value }));
   };
 
   const handleSubmit = event => {
@@ -46,15 +45,15 @@ function GroupFilterWhereWhen(props = {}) {
     >
       <Box textAlign="center">
         <Box as="h2">Find your Community</Box>
-        <Box as="p" color="subdued" mb="l">
-          Where and when?
-        </Box>
       </Box>
       <Box mb="base">
+        <Box as="p" color="subdued" mb="base" textAlign="center">
+          What campus do you prefer?
+        </Box>
         <Select
           id="campuses"
           name="campuses"
-          onChange={handleChange}
+          onChange={handleCampusChange}
           defaultValue={filtersState.values.campuses[0] || ''}
         >
           <Select.Option value="">Select...</Select.Option>
@@ -66,20 +65,37 @@ function GroupFilterWhereWhen(props = {}) {
         </Select>
       </Box>
       <Box mb="l">
-        <Box as="p" color="subdued" mb="s">
-          Preferred meeting days
+        <Box as="p" color="subdued" mb="base" textAlign="center">
+          What days can you meet?
         </Box>
-        {filtersState.options.days.map(value => (
-          <Checkbox
-            key={value}
-            id={value}
-            label={value}
-            value={value}
-            name="days"
-            onChange={handleChange}
-            checked={filtersState.values.days.includes(value)}
-          />
-        ))}
+        <Box
+          display="grid"
+          width="100%"
+          gridTemplateColumns="repeat(7, 1fr)"
+          gridColumnGap="xs"
+        >
+          {filtersState.options.days.map(value => (
+            <Button
+              key={value}
+              variant="secondary"
+              status={
+                filtersState.values.days.includes(value) ? 'SELECTED' : 'IDLE'
+              }
+              onClick={event => {
+                event.preventDefault();
+                handleDayChange(value);
+              }}
+              mb="xs"
+              px="0"
+            >
+              {value[0]}
+              {/* Gross, but quick way to abbreviate for mobile... */}
+              <Box as="span" display={{ _: 'none', md: 'inline' }}>
+                {value.slice(1)}
+              </Box>
+            </Button>
+          ))}
+        </Box>
       </Box>
       <Button type="submit">Continue</Button>
     </Box>
