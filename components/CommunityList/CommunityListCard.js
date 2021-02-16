5eae8f2c07effee5d9558c72a8ff36057bf94286
Ryan Davidson <ryan@rkdavidson.com>
2021-02-16 11:54:59 -0500

    Use transformImageUrl in Community cards

--- components/CommunityList/CommunityListCard.js
+++ components/CommunityList/CommunityListCard.js
@@ -1,13 +1,10 @@
-import isString from 'lodash/isString';
-
 import { DefaultCard } from 'ui-kit';
+import { transformImageUrl } from 'utils';
 
 export default function CommunityListCard(props = {}) {
   // Note: Safe assumption that `props.src` is an image from our own
-  // API and Cloudinary setup... so append parameters to optimize loading
-  const coverImage = isString(props.coverImage)
-    ? `${props.coverImage}&w=768`
-    : props.coverImage;
+  // API and Cloudinary setup... so append transform parameters to optimize.
+  const coverImage = transformImageUrl(props.coverImage, { w: 768 });
 
   return <DefaultCard {...props} coverImage={coverImage} />;
 }
