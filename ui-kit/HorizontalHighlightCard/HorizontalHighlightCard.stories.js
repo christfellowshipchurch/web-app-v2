0980c4e8baffb4d5796f4c3b4bed96ad69271316
Ryan Davidson <ryan@rkdavidson.com>
2021-02-18 11:56:35 -0500

    Flesh out storybook cases for clarity

--- ui-kit/HorizontalHighlightCard/HorizontalHighlightCard.stories.js
+++ ui-kit/HorizontalHighlightCard/HorizontalHighlightCard.stories.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import Carousel from 'react-multi-carousel';
 
-import { Box, CardGrid } from '..';
+import { CardGrid } from '..';
 import HorizontalHighlightCard from './HorizontalHighlightCard';
 
 export default {
@@ -13,7 +12,7 @@ const card = cardType => {
   return {
     mx: 'xs',
     coverImage: 'https://source.unsplash.com/random/1000x1000',
-    description: 'This is a default Horiztontal Highlight Card',
+    description: 'This is a default Horizontal Highlight Card',
     title: 'Horizontal Highlight Card',
     coverImageOverlay: true,
     type: cardType,
@@ -21,12 +20,23 @@ const card = cardType => {
 };
 
 export const Small = () => (
-  <CardGrid columns={'4'} maxWidth={1000}>
-    <HorizontalHighlightCard {...card('HIGHLIGHT_SMALL')} />
-    <HorizontalHighlightCard {...card('HIGHLIGHT_SMALL')} />
-    <HorizontalHighlightCard {...card('HIGHLIGHT_SMALL')} />
-    <HorizontalHighlightCard {...card('HIGHLIGHT_SMALL')} />
-  </CardGrid>
+  <>
+    <CardGrid columns={'4'} maxWidth={1000} mb="xl">
+      <HorizontalHighlightCard {...card('HIGHLIGHT_SMALL')} />
+      <HorizontalHighlightCard {...card('HIGHLIGHT_SMALL')} />
+      <HorizontalHighlightCard {...card('HIGHLIGHT_SMALL')} />
+      <HorizontalHighlightCard {...card('HIGHLIGHT_SMALL')} />
+    </CardGrid>
+    <CardGrid columns={'3'} maxWidth={1000} mb="xl">
+      <HorizontalHighlightCard {...card('HIGHLIGHT_SMALL')} />
+      <HorizontalHighlightCard {...card('HIGHLIGHT_SMALL')} />
+      <HorizontalHighlightCard {...card('HIGHLIGHT_SMALL')} />
+    </CardGrid>
+    <CardGrid columns={'2'} maxWidth={1000} mb="xl">
+      <HorizontalHighlightCard {...card('HIGHLIGHT_SMALL')} />
+      <HorizontalHighlightCard {...card('HIGHLIGHT_SMALL')} />
+    </CardGrid>
+  </>
 );
 
 export const Medium = () => (
