319494a1f2b51ee363ad8cfc2eae4abdd7d42faa
Ryan Davidson <rkdavidson@users.noreply.github.com>
2021-02-25 17:48:37 -0500

    Update preferences and subPreferences cards (#36)
    * Community pages card light refactoring
    
    * Flesh out storybook cases for clarity
    
    Co-authored-by: Jordan Wade <jjwade31@gmail.com>

--- components/CommunityList/CommunityListCard.js
+++ components/CommunityList/CommunityListCard.js
@@ -1,13 +1,5 @@
-import isString from 'lodash/isString';
-
-import { DefaultCard } from 'ui-kit';
+import { HorizontalHighlightCard } from 'ui-kit';
 
 export default function CommunityListCard(props = {}) {
-  // Note: Safe assumption that `props.src` is an image from our own
-  // API and Cloudinary setup... so append parameters to optimize loading
-  const coverImage = isString(props.coverImage)
-    ? `${props.coverImage}&w=768`
-    : props.coverImage;
-
-  return <DefaultCard {...props} coverImage={coverImage} />;
+  return <HorizontalHighlightCard as="a" type="HIGHLIGHT_SMALL" {...props} />;
 }
