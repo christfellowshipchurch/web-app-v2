6e26ba354a0cd7f0c68aa54b7091672b4154020c
Jordan Wade <jjwade31@gmail.com>
2021-03-01 09:43:12 -0600

    Refine no results for group search (#48)
    Co-authored-by: Ryan Davidson <rkdavidson@users.noreply.github.com>

--- pages/community/search/index.js
+++ pages/community/search/index.js
@@ -9,9 +9,14 @@ import {
   SEO,
   SearchField,
   GroupsResultsList,
+  CustomLink,
 } from 'components';
 
-import { useGroupFilters, update } from 'providers/GroupFiltersProvider';
+import {
+  useGroupFilters,
+  update,
+  resetValues,
+} from 'providers/GroupFiltersProvider';
 import { useModalState } from 'providers/ModalProvider';
 import { GroupsProvider } from 'providers';
 import { useSearchGroups, useForm } from 'hooks';
@@ -32,7 +37,7 @@ export default function CommunitySearch() {
   const showEmptyState = !loading && !hasResults;
   const hasMorePages = groups?.length < data?.searchGroups?.totalResults;
 
-  const { values, handleSubmit, handleChange, setValues } = useForm(() => {
+  const { values, handleSubmit, handleChange, setValues, reset } = useForm(() => {
     router.push({
       pathname: `/community/search`,
       query: filtersState.valuesSerialized,
@@ -78,6 +83,15 @@ export default function CommunitySearch() {
   const handleClick = event => {
     filtersDispatch(update({ text: [values.text] }));
   };
+
+  function handleClearAllClick(event) {
+    event.preventDefault();
+    // Update search page URL
+    router.push({
+      pathname: `/community/search`,
+    });
+    filtersDispatch(resetValues(), reset());
+  }
 
   return (
     <>
@@ -128,8 +142,30 @@ export default function CommunitySearch() {
           />
 
           {showEmptyState && (
-            <Box display="flex" justifyContent="center" my="xxl" pb="xxl">
-              <Box>No Groups matched your search criteria.</Box>
+            <Box my="xxl" pb="xxl" textAlign="center">
+              <Box as="h2">Looks like we couldn't find any results</Box>
+              <Box mb="base">
+                Consider reducing the number of filters or modifing your search
+                criteria.
+              </Box>
+              <Box
+                display="flex"
+                alignItems="center"
+                flexDirection="column"
+                mt="l"
+                textAlign="center"
+              >
+                <Button
+                  variant="secondary"
+                  onClick={handleClearAllClick}
+                  mb="s"
+                >
+                  Clear Search
+                </Button>
+                <CustomLink href="https://rock.gocf.org/page/2113">
+                  Need help?
+                </CustomLink>
+              </Box>
             </Box>
           )}
 
