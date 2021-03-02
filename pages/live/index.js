aa104b8953f9a3ef6fffa8846d0288b71f92c6f1
Ryan Davidson <ryanrkd2112@gmail.com>
2021-03-02 16:10:43 -0500

    Show list of livestreams

--- pages/live/index.js
+++ pages/live/index.js
@@ -1,17 +1,36 @@
 import { useLiveStreamsQuery } from 'hooks';
+import { slugify } from 'utils';
 
-import { Box } from 'ui-kit';
-import { Layout } from 'components';
+import { Box, DefaultCard } from 'ui-kit';
+import { CustomLink, Layout } from 'components';
 
 export default function Live() {
-  const { liveStreams } = useLiveStreamsQuery();
+  const { liveStreams = [] } = useLiveStreamsQuery();
 
   return (
     <Layout title="Live">
       <Box as="h1" mb="l">
-        Live
+        LiveStreams
       </Box>
-      <pre>{JSON.stringify(liveStreams, null, 2)}</pre>
+      <Box display="grid" gridTemplateColumns={{ __: '1fr', md: '1fr 1fr' }}>
+        {!liveStreams.length && <Box as="p">No livestreams</Box>}
+        {liveStreams.map(liveStream => (
+          <CustomLink
+            key={liveStream.id}
+            as="a"
+            href={`/live/${slugify(liveStream.relatedNode?.title)}`}
+            Component={DefaultCard}
+            coverImage={liveStream.relatedNode?.coverImage?.sources[0]?.uri}
+            coverImageOverlay={true}
+            coverImageTitle={liveStream.relatedNode?.title}
+            coverImageDescription={liveStream.relatedNode?.summary}
+            m="base"
+            display="block"
+            height={{ __: 256, md: 384 }}
+            maxWidth={{ __: 384, md: 512 }}
+          />
+        ))}
+      </Box>
     </Layout>
   );
 }
