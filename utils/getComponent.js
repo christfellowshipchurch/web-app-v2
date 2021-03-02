c0032a7a2d0af9484a8282033bf1f79ca334b07a
Jordan Wade <jjwade31@gmail.com>
2021-03-02 15:42:26 -0600

    Update get component to return meaningful error

--- utils/getComponent.js
+++ utils/getComponent.js
@@ -1,13 +1,19 @@
-// grabs __typename from edge and matches it 
+// grabs __typename from edge and matches it
 // with component inside of the `Components` object
 function getComponent(edge, Components) {
-    let { __typename, id } = edge;
-  
-    if (!__typename && id) {
-      [__typename] = id.split(':');
-    }
-  
-    return Components[__typename];
+  let { __typename, id } = edge;
+
+  if (!__typename && id) {
+    [__typename] = id.split(':');
   }
 
-  export default getComponent
+  // Catch unmapped features
+  if (!Components[__typename]) {
+    console.warn(`No component found for __typename: "${__typename}"`);
+    return () => null;
+  }
+
+  return Components[__typename];
+}
+
+export default getComponent;
