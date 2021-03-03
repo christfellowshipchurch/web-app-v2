1fde8d8e5efc0c00a12fa92d0d2515c37e82b53b
Ryan Davidson <ryanrkd2112@gmail.com>
2021-03-03 16:00:57 -0500

    Use Video component in LiveStreamSingle

--- components/LiveStreamSingle/LiveStreamSingle.js
+++ components/LiveStreamSingle/LiveStreamSingle.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { ContentLayout } from 'components';
+import { ContentLayout, Video } from 'components';
+import { Box } from 'ui-kit';
 
 function LiveStreamSingle(props = {}) {
+  // const videoSrc = props.data?.media?.sources[0].uri;
+
+  // 👇 OVERRIDE FOR TESTING
+  const videoSrc =
+    'http://amssamples.streaming.mediaservices.windows.net/91492735-c523-432b-ba01-faba6c2206a2/AzureMediaServicesPromo.ism/manifest(format=m3u8-aapl)';
+
   return (
-    <ContentLayout
-      title={props.data?.relatedNode?.title}
-      summary={props.data?.relatedNode?.summary}
-      coverImage={props.data?.relatedNode?.coverImage?.sources[0]?.uri}
-    />
+    <>
+      <ContentLayout
+        title={props.data?.relatedNode?.title}
+        summary={props.data?.relatedNode?.summary}
+        renderA={() => (
+          <Box
+            mb="l"
+            background={`url(${props.data?.relatedNode?.coverImage?.sources[0]?.uri}) center center no-repeat`}
+            backgroundSize="cover"
+          >
+            <Video autoPlay={true} src={videoSrc} />
+          </Box>
+        )}
+      />
+    </>
   );
 }
 
