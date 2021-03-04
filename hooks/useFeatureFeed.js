be85688920d5f753f676eb4a14e9e2cb766004e5
Jordan Wade <jjwade31@gmail.com>
2021-03-04 11:38:01 -0600

    Update useFeatureFeed to import fragments

--- hooks/useFeatureFeed.js
+++ hooks/useFeatureFeed.js
@@ -1,10 +1,18 @@
 import { gql, useQuery } from '@apollo/client';
 
+import {
+  LITE_FEATURES_FRAGMENT,
+  ACTION_BAR_FEATURE_FRAGMENT,
+  AVATAR_LIST_FEATURE_FRAGMENT,
+  RELATED_FEATURE_NODE_FRAGMENT,
+  THEME_FRAGMENT,
+} from 'fragments';
+
 export const GET_FEATURE_FEED = gql`
   query getFeatureFeed {
     userFeedFeatures {
       ...LiteFeaturesFragment
-      ...ActionBarFeatureFragment 
+      ...ActionBarFeatureFragment
       ...AvatarListFeatureFragment
       ... on HorizontalCardListFeature {
         cardType
@@ -18,120 +26,12 @@ export const GET_FEATURE_FEED = gql`
       }
     }
   }
-  
-  fragment LiteFeaturesFragment on Feature {
-    id
-    __typename
-    ... on VerticalCardListFeature {
-      isFeatured
-      title
-      subtitle
-    }
-    ... on HorizontalCardListFeature {
-      title
-      subtitle
-    }
-    ... on ActionListFeature {
-      title
-      subtitle
-    }
-    ... on HeroListFeature {
-      title
-      subtitle
-    }
-    ... on PrayerListFeature {
-      title
-      subtitle
-      isCard
-    }
-    ... on TextFeature {
-      body
-      sharing {
-        message
-      }
-    }
-    ... on ScriptureFeature {
-      # The whole fragment is currently included b/c these nodes don't fetch their own content.
-      sharing {
-        message
-      }
-      scriptures {
-        id
-        html
-        reference
-        copyright
-        version
-      }
-    }
-    ... on WebviewFeature {
-      # The whole fragment is currently included b/c these nodes don't fetch their own content.
-      linkText
-      title
-      url
-    }
-  }
-
-  fragment ActionBarFeatureFragment on ActionBarFeature {
-    id
-    actions {
-      title
-      icon
-      action
-      relatedNode {
-        ...RelatedFeatureNodeFragment
-      }
-    }
-  }
-
-  fragment AvatarListFeatureFragment on AvatarListFeature {
-    id
-    order
-    people {
-      id
-      firstName
-      lastName
-      photo {
-        uri
-      }
-      campus {
-        id
-        name
-      }
-    }
-    isCard
-    primaryAction {
-      action
-      icon
-      theme {
-        ...ThemeFragment
-      }
-      relatedNode {
-        ...RelatedFeatureNodeFragment
-      }
-    }
-  }
-
-  fragment ThemeFragment on Theme {
-    type
-    colors {
-      primary
-      secondary
-      screen
-      paper
-      alert
-    }
-  }
-
-  fragment RelatedFeatureNodeFragment on Node {
-    id
-    ... on Url {
-      url
-    }
-    ... on ContentChannel {
-      name
-    }
-  }
-  `;
+  ${LITE_FEATURES_FRAGMENT}
+  ${ACTION_BAR_FEATURE_FRAGMENT}
+  ${AVATAR_LIST_FEATURE_FRAGMENT}
+  ${RELATED_FEATURE_NODE_FRAGMENT}
+  ${THEME_FRAGMENT}
+`;
 
 function useFeatureFeed(options = {}) {
   const query = useQuery(GET_FEATURE_FEED, options);
