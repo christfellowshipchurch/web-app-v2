59b1db5704e898df6306c05f7ac3f1d20fc5f0cd
Ryan Davidson <ryanrkd2112@gmail.com>
2021-03-08 16:35:21 -0500

    Clean up Live

--- components/LiveStreamSingle/Live.js
+++ components/LiveStreamSingle/Live.js
@@ -12,40 +12,38 @@ function Live(props = {}) {
   //   'http://amssamples.streaming.mediaservices.windows.net/91492735-c523-432b-ba01-faba6c2206a2/AzureMediaServicesPromo.ism/manifest(format=m3u8-aapl)';
 
   return (
-    <>
-      <ContentLayout
-        title={props.data?.relatedNode?.title}
-        summary={props.data?.relatedNode?.summary}
-        renderA={() => (
-          <Box
-            mb="l"
-            background={`url(${props.data?.relatedNode?.coverImage?.sources[0]?.uri}) center center no-repeat`}
-            backgroundSize="cover"
-          >
-            <Video src={videoSrc} autoPlay={false} muted={true} />
-          </Box>
-        )}
-        renderC={() => (
-          <Box
-            as="h4"
-            display="inline-block"
-            bg="live"
-            color="white"
-            px="s"
-            borderRadius="s"
-            fontWeight="bold"
-          >
-            Live Now
-          </Box>
-        )}
-        renderD={() => (
-          <Box as="pre" fontSize="s">
-            {JSON.stringify(props.data, null, 2)}
-            {JSON.stringify(props.metaData, null, 2)}
-          </Box>
-        )}
-      />
-    </>
+    <ContentLayout
+      title={props.data?.relatedNode?.title}
+      summary={props.data?.relatedNode?.summary}
+      renderA={() => (
+        <Box
+          mb="l"
+          background={`url(${props.data?.relatedNode?.coverImage?.sources[0]?.uri}) center center no-repeat`}
+          backgroundSize="cover"
+        >
+          <Video src={videoSrc} />
+        </Box>
+      )}
+      renderC={() => (
+        <Box
+          as="h4"
+          display="inline-block"
+          bg="live"
+          color="white"
+          px="s"
+          borderRadius="s"
+          fontWeight="bold"
+        >
+          Live Now
+        </Box>
+      )}
+      renderD={() => (
+        <Box as="pre" fontSize="s">
+          {JSON.stringify(props.data, null, 2)}
+          {JSON.stringify(props.metaData, null, 2)}
+        </Box>
+      )}
+    />
   );
 }
 
