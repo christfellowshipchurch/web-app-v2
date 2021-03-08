50a68e047595c12c740492997d934fb0cacd403e
Ryan Davidson <ryanrkd2112@gmail.com>
2021-03-08 15:20:05 -0500

    PreLive state updates

--- components/LiveStreamSingle/PreLive.js
+++ components/LiveStreamSingle/PreLive.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { format } from 'date-fns';
 
+import { parseLiveStreamDates } from 'utils';
 import { ContentLayout } from 'components';
 import { Box } from 'ui-kit';
 
 function PreLive(props = {}) {
+  const parsed = parseLiveStreamDates(props.data);
+  let startFormatted = parsed.startDate
+    ? format(parsed.startDate, `EEEE, MMMM dd 'at' h:mm a`)
+    : 'null';
+  let endFormatted = parsed.endDate
+    ? format(parsed.endDate, `EEEE, MMMM dd 'at' h:mm a`)
+    : 'null';
+
   return (
     <>
       <ContentLayout
         title={props.data?.relatedNode?.title}
-        summary={props.data?.relatedNode?.summary}
+        summary="Coming Soon"
         coverImage={props.data?.relatedNode?.coverImage?.sources[0]?.uri}
         renderD={() => (
-          <Box as="p" fontSize="s">
-            Coming soon
+          <Box as="p">
+            <b>Starts:</b> {startFormatted}
+            <br />
+            <b>Ends:</b> {endFormatted}
           </Box>
         )}
       />
