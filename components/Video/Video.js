09f74fea55040beea9a52e151b686e1745753514
Ryan Davidson <ryanrkd2112@gmail.com>
2021-03-10 12:23:07 -0500

    Video component PR prep

--- components/Video/Video.js
+++ components/Video/Video.js
@@ -1,8 +1,10 @@
 import muxjs from 'mux.js';
 import dynamic from 'next/dynamic';
+
 import 'shaka-player/dist/controls.css';
 
 // Mux is used to deal with encodings and low-level video nonsense.
+// Without it, streams are unlikely to work on iOS and Safari.
 if (typeof window !== 'undefined') {
   window.muxjs = muxjs;
 }
@@ -11,7 +13,7 @@ if (typeof window !== 'undefined') {
 const ShakaPlayer = dynamic(() => import('shaka-player-react'), { ssr: false });
 
 export default function Video(props = {}) {
-  return <ShakaPlayer autoPlay {...props} />;
+  return <ShakaPlayer {...props} />;
 }
 
 Video.propTypes = {
