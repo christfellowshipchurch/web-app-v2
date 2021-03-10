d32818ca5d0a53c59ae45b18549ed8809cfbb5f7
Luke Mommaerts <luke@differential.com>
2021-03-10 14:17:24 -0500

    Update Women page with Rock data

--- pages/connect/women.js
+++ pages/connect/women.js
@@ -10,12 +10,36 @@ import {
   Quote,
 } from 'components';
 import { Box, CardGrid, Heading, Text } from 'ui-kit';
-import { noop } from 'utils';
+import { getChildrenByType, noop } from 'utils';
 import { initializeApollo } from 'lib/apolloClient';
 import { GET_CONTENT_ITEM } from 'hooks/useContentItem';
+import IDS from 'config/ids';
 
 export default function Women(props) {
-  const article = props.articles?.[0];
+  const pageChildren = props.page?.childContentItemsConnection?.edges;
+  const quote = getChildrenByType(pageChildren, IDS.QUOTES)[0];
+  const articles = getChildrenByType(pageChildren, IDS.ARTICLES);
+
+  const explorations = articles.map(({ node: article }) => ({
+    title: article.title,
+    description: article.summary,
+    url: '/',
+    urlText: 'Learn More',
+    imageSrc: article.images?.[0]?.sources?.[0]?.uri,
+  }));
+
+  const exploreColumns = [[], []];
+
+  for (let i = 0; i < explorations.length; ++i) {
+    if (i < explorations.length / 2) {
+      exploreColumns[0].push(explorations[i]);
+    } else {
+      exploreColumns[1].push(explorations[i]);
+    }
+  }
+
+  const exploreRowCount = exploreColumns[0].length;
+
   return (
     <Layout title="Connect - Women">
       <MainPhotoHeader
@@ -57,30 +81,18 @@ export default function Women(props) {
         columns="2"
         breakpoints={[{ breakpoint: 'lg', columns: 1 }]}
       >
-        <ArticleLinks>
-          {Array.from(Array(2)).map(() => (
-            <ArticleLink
-              title={article?.title}
-              description="At Long Hollow Weekday Preschool, we are committed to creating a nurturing environment for preschoolers to grow."
-              url="/"
-              urlText="Learn More"
-              imageSrc={article?.coverImage?.sources?.[0]?.uri}
-              mb="s"
-            />
-          ))}
-        </ArticleLinks>
-        <ArticleLinks>
-          {Array.from(Array(2)).map(() => (
-            <ArticleLink
-              title={article?.title}
-              description="At Long Hollow Weekday Preschool, we are committed to creating a nurturing environment for preschoolers to grow."
-              url="/"
-              urlText="Learn More"
-              imageSrc={article?.coverImage?.sources?.[0]?.uri}
-              mb="s"
-            />
-          ))}
-        </ArticleLinks>
+        {exploreColumns.map((exploreColumn, i) => (
+          <CardGrid
+            key={i}
+            gridRowGap="xs"
+            columns="1"
+            gridTemplateRows={`repeat(${exploreRowCount}, 1fr)`}
+          >
+            {exploreColumn.map((explore, j) => (
+              <ArticleLink key={i + j} {...explore} />
+            ))}
+          </CardGrid>
+        ))}
       </CardGrid>
       <CardGrid
         px="xxl"
@@ -127,7 +139,7 @@ export default function Women(props) {
                   lineHeight="27px"
                   fontWeight="400"
                 >
-                  Woman Story
+                  {quote?.node?.title}
                 </Heading>
               </Box>
             }
@@ -142,12 +154,10 @@ export default function Women(props) {
                 opacity="60%"
                 textAlign="left"
               >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit,
-                arcu consequat vestibulum amet. Velit nunc augue a blandit diam.
-                Malesuada eget faucibus amet hac.
+                {quote?.node?.summary}
               </Text>
             }
-            avatar={props.quote?.coverImage?.sources?.[0]?.uri}
+            avatar={quote?.node?.coverImage?.sources?.[0]?.uri}
           />
         </CardGrid>
       </CardGrid>
