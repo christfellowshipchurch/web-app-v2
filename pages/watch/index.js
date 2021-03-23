2589225a01a720dc415f3845788563d615945fe0
Luke Mommaerts <luke@differential.com>
2021-03-23 15:34:00 -0400

    Add CTA Links to Watch page

--- pages/watch/index.js
+++ pages/watch/index.js
@@ -1,21 +1,24 @@
-import { LargeImage, Layout, MainPhotoHeader } from 'components';
+import { LargeImage, Layout, MainPhotoHeader, MarketingHeadline, PageSplit } from 'components';
 import { initializeApollo } from 'lib/apolloClient';
 import { useRouter } from 'next/router';
 import IDS from 'config/ids';
 import { Box, CardGrid, Heading } from 'ui-kit';
 import { GET_MESSAGE_SERIES } from 'hooks/useMessageSeries';
 import { flatten } from 'lodash';
+import { GET_CONTENT_CHANNEL } from 'hooks/useContentChannel';
 
 function getChannelId(id) {
   return `ContentChannel:${id}`;
 }
 
-export default function Watch({ series }) {
+export default function Watch({ series, watch }) {
   const router = useRouter();
   const allSeries = flatten(series).filter(
     ({ node: seriesNode }) =>
       seriesNode.childContentItemsConnection?.edges.length
   );
+
+  const ctas = watch?.node?.ctaLinks;
   return (
     <Layout title="Watch">
       <MainPhotoHeader src="/watch.jpeg" title="LH Watch" subtitle="" />
@@ -72,12 +75,38 @@ export default function Watch({ series }) {
           </Box>
         ))}
       </CardGrid>
+      {ctas?.length ? (
+        <>
+          <PageSplit title="Other ways to watch" />
+          <CardGrid
+            columns="1"
+            gridRowGap="m"
+          >
+            {ctas.map((cta, i) => (
+              <MarketingHeadline
+                src={cta.coverImage?.sources?.[0]?.uri}
+                title={cta.title}
+                description={cta.description}
+                justify={i % 2 ? 'left' : 'right'}
+                actions={[{ label: cta.buttonText, onClick: () => router.push(cta.buttonLink)}]}
+              />
+            ))}
+          </CardGrid>
+        </>
+      ) : null}
     </Layout>
   );
 }
 
 export async function getServerSideProps() {
   const apolloClient = initializeApollo();
+
+  const watchRequest = await apolloClient.query({
+    query: GET_CONTENT_CHANNEL,
+    variables: {
+      itemId: getChannelId(IDS.WATCH_PAGES)
+    }
+  });
 
   const seriesRequests = Object.values(IDS.MESSAGES).map(async id =>
     apolloClient.query({
@@ -96,6 +125,7 @@ export async function getServerSideProps() {
       series: series.map(
         serie => serie?.data?.node?.childContentItemsConnection?.edges
       ),
+      watch: watchRequest?.data?.node?.childContentItemsConnection?.edges?.[0]
     },
   };
 }
