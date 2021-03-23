80c487649d6c9b0d47805ec8191fcb912a030968
Ryan Davidson <ryanrkd2112@gmail.com>
2021-03-23 14:19:14 -0400

    Add Stream CSS again

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,9 @@ import Head from 'next/head';
 import configureNProgress from 'config/nprogress';
 import { AppProvider } from 'providers';
 
+import 'stream-chat-react/dist/css/index.css';
+import './StreamChatOverrides.css';
+
 // Tracks the route changes and adds a bar to the top.
 configureNProgress();
 
