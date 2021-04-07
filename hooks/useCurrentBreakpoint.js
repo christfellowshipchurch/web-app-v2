c9c430d5bf051e04becb17ef086eea3508d991c5
Ryan Davidson <ryanrkd2112@gmail.com>
2021-04-07 14:10:46 -0400

    Create useCurrentBreakpoint hook

---
+++ hooks/useCurrentBreakpoint.js
@@ -0,0 +1,82 @@
+import { useState, useRef, useEffect } from 'react';
+
+import { theme } from 'ui-kit';
+
+const { breakpoints } = theme;
+
+const breakpointNames = Object.keys(breakpoints);
+const defaultBreakpoint = breakpointNames[0];
+
+export default function useCurrentBreakpoint() {
+  const [currentBreakpoint, setCurrentBreakpoint] = useState(defaultBreakpoint);
+  const matchers = useRef([]);
+  const isClient = typeof window !== undefined;
+
+  useEffect(() => {
+    if (!isClient) {
+      return null;
+    }
+
+    console.group('Initializing matchers');
+    matchers.current = [];
+    let initialBreakpointName = breakpointNames[0];
+    let cleanupFunctions = [];
+
+    // For each breakpoint (after the first)...
+    for (let index = 1; index < breakpointNames.length; index++) {
+      const name = breakpointNames[index];
+      console.log(`- index: ${index} => ${name} => ${breakpoints[name]}`);
+
+      const matcher = window.matchMedia(`(min-width: ${breakpoints[name]})`);
+
+      // Is this breakpoint active?
+      if (matcher.matches) {
+        console.log(`breakpoint ${name} matched!`);
+        initialBreakpointName = name;
+      }
+
+      // Listen for changes to this breakpoint `matches` status
+      const handleMatchesChange = () => {
+        if (matcher.matches) {
+          console.log(
+            `%cMOVED UP -----> name: ${name} (${matcher.matches})`,
+            'color: orange'
+          );
+          // Moved up a breakpoint
+          setCurrentBreakpoint(name);
+        } else {
+          console.log(
+            `%cMOVED DOWN <----- name: ${name} (${matcher.matches})`,
+            'color: orange'
+          );
+          // Moved down a breakpoint
+          setCurrentBreakpoint(breakpointNames[Math.max(0, index - 1)]);
+        }
+      };
+
+      matcher.addEventListener('change', handleMatchesChange);
+      cleanupFunctions.push(() => {
+        matcher.removeEventListener('change', handleMatchesChange);
+      });
+
+      matchers.current.push(matcher);
+    }
+
+    setCurrentBreakpoint(initialBreakpointName);
+    console.log('--> initialBreakpointName:', initialBreakpointName);
+    console.groupEnd();
+
+    return () => {
+      cleanupFunctions.forEach(cleanupFunction => cleanupFunction());
+      matchers.current = [];
+    };
+  }, [isClient]);
+
+  return {
+    name: currentBreakpoint,
+    isSmall: currentBreakpoint === 'sm',
+    isMedium: currentBreakpoint === 'md',
+    isLarge: currentBreakpoint === 'lg',
+    isXLarge: currentBreakpoint === 'xl',
+  };
+}
