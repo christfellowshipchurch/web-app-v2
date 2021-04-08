69ae9f1339210f06104200d1c10ef9fb798b5d98
Jordan Wade <jjwade31@gmail.com>
2021-04-07 22:12:41 -0500

    Remove unused button import

--- components/EventSingle/EventSingle.js
+++ components/EventSingle/EventSingle.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Button } from 'ui-kit';
 import { ContentLayout, Share } from 'components';
 
 import EventGroupings from './EventGroupings';
