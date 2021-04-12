ae70e880ebcf244079ed77529536b04820af20ab
Luke Mommaerts <luke@differential.com>
2021-04-12 11:05:03 -0400

    Prevent empty column if non featured items empty

--- components/Dropdowns/PageDropdown.js
+++ components/Dropdowns/PageDropdown.js
@@ -13,61 +13,72 @@ export default function PageDropdown({
 }) {
   const router = useRouter();
 
-  const numColumns = Math.ceil(featuredItems.length / 2) + 1;
+  const hasNonFeaturedItems = Boolean(nonFeaturedItems.length);
+
+  const numColumns =
+    Math.ceil(featuredItems.length / 2) + Number(hasNonFeaturedItems);
 
   return (
     <Dropdowns.Container {...props}>
       <CardGrid columns={numColumns} gridColumnGap="base">
-        {numColumns > 1
-          ? [...Array(numColumns - 1).keys()].map(i => {
-              const items = featuredItems.slice(i * 2, i * 2 + 2);
+        {featuredItems.length
+          ? [...Array(numColumns - Number(hasNonFeaturedItems)).keys()].map(
+              i => {
+                const items = featuredItems.slice(i * 2, i * 2 + 2);
 
-              return (
-                <Box key={i} display="flex" flexDirection="column">
-                  {items.map((item, i) => (
-                    <Dropdowns.FeaturedItem key={item.id}>
-                      <Photo
-                        height="226px"
-                        src={item.coverImage?.sources[0]?.uri}
-                        inner={
-                          <Heading fontWeight="600" variant="h2" color="white">
-                            {item.title}
-                          </Heading>
-                        }
-                        overlay={{
-                          color: theme.colors.almost_black,
-                          opacity: '30%',
-                        }}
-                        onClick={() =>
-                          router.push(`${baseRoute}/${getIdSuffix(item.id)}`)
-                        }
-                      />
-                    </Dropdowns.FeaturedItem>
-                  ))}
-                </Box>
-              );
-            })
-          : null}
-        <Box display="flex" flexDirection="column" width="100%">
-          {nonFeaturedItems.map((item, i) => (
-            <Dropdowns.NonFeaturedItem
-              key={i}
-              onClick={() =>
-                router.push(`${baseRoute}/${getIdSuffix(item.id)}`)
+                return (
+                  <Box key={i} display="flex" flexDirection="column">
+                    {items.map((item, i) => (
+                      <Dropdowns.FeaturedItem key={item.id}>
+                        <Photo
+                          height="226px"
+                          src={item.coverImage?.sources[0]?.uri}
+                          inner={
+                            <Heading
+                              fontWeight="600"
+                              variant="h2"
+                              color="white"
+                            >
+                              {item.title}
+                            </Heading>
+                          }
+                          overlay={{
+                            color: theme.colors.almost_black,
+                            opacity: '30%',
+                          }}
+                          onClick={() =>
+                            router.push(`${baseRoute}/${getIdSuffix(item.id)}`)
+                          }
+                        />
+                      </Dropdowns.FeaturedItem>
+                    ))}
+                  </Box>
+                );
               }
-            >
-              <Heading
-                fontSize="l"
-                lineHeight="s"
-                fontWeight="500"
-                color="white"
+            )
+          : null}
+        {nonFeaturedItems.length ? (
+          <Box display="flex" flexDirection="column" width="100%">
+            {nonFeaturedItems.map((item, i) => (
+              <Dropdowns.NonFeaturedItem
+                key={i}
+                onClick={() =>
+                  router.push(`${baseRoute}/${getIdSuffix(item.id)}`)
+                }
               >
-                {item.title}
-              </Heading>
-              <ArrowCircleRight size="32" color="white" />
-            </Dropdowns.NonFeaturedItem>
-          ))}
-        </Box>
+                <Heading
+                  fontSize="l"
+                  lineHeight="s"
+                  fontWeight="500"
+                  color="white"
+                >
+                  {item.title}
+                </Heading>
+                <ArrowCircleRight size="32" color="white" />
+              </Dropdowns.NonFeaturedItem>
+            ))}
+          </Box>
+        ) : null}
       </CardGrid>
     </Dropdowns.Container>
   );
