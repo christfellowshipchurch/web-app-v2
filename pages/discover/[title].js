7f2b5c5cba0a464b6a9a943de36cf0c2a66dd5da
Jordan Wade <jjwade31@gmail.com>
2021-04-16 14:52:03 -0500

    Add back button to discover (#83)
    * Add back button to see more discover
    
    * Limit see more query

--- pages/discover/[title].js
+++ pages/discover/[title].js
@@ -4,16 +4,16 @@ import startCase from 'lodash/startCase';
 import { getURLFromType } from 'utils';
 import { useDiscoverFilterCategoriesPreview } from 'hooks';
 
-import { Box, DefaultCard, CardGrid } from 'ui-kit';
+import { Box, DefaultCard, CardGrid, Icon, Button } from 'ui-kit';
 import { Layout, CustomLink } from 'components';
 
 export default function Content(props) {
-  const { query } = useRouter();
+  const { query, back } = useRouter();
   const type = 'UniversalContentItem';
   const contentId = type.concat(':', query?.id);
 
   const { categories } = useDiscoverFilterCategoriesPreview({
-    variables: { id: contentId },
+    variables: { id: contentId, first: 21 },
     fetchPolicy: 'cache-and-network',
   });
 
@@ -21,8 +21,18 @@ export default function Content(props) {
 
   return (
     <Layout title={startCase(query?.title)}>
-      <Box as="h1" mb="l">
-        {startCase(query?.title)}
+      <Box
+        alignItems="center"
+        display="flex"
+        justifyContent="space-between"
+        mb="l"
+      >
+        <Box as="h1" mb="0">
+          {startCase(query?.title)}
+        </Box>
+        <Button variant="link" onClick={() => back()} pr="0">
+          <Icon name="angleLeft" /> Back
+        </Button>
       </Box>
       <CardGrid columns="3" mb="xl">
         {content.map((n, i) => (
