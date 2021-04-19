f447f9ffed88efb582edf2ad1b9190181e7d97fc
Jordan Wade <jjwade31@gmail.com>
2021-04-19 16:29:57 -0500

    Add author to articles (#84)
    * Fix item id for content items
    
    * Add authors to content articles
    
    * Simplify discover urls
    
    Co-authored-by: Ryan Davidson <rkdavidson@users.noreply.github.com>

--- components/ContentSingle/ContentSingle.js
+++ components/ContentSingle/ContentSingle.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { format } from 'date-fns';
 
-import { Box, Button } from 'ui-kit';
+import { Box, Avatar } from 'ui-kit';
 import { ContentLayout, Share } from 'components';
 
 function ContentSingle(props = {}) {
@@ -11,11 +12,34 @@ function ContentSingle(props = {}) {
       summary={props.data.schedule?.friendlyScheduleText}
       coverImage={props.data?.coverImage?.sources[0]?.uri}
       renderC={() => (
-        <Box justifySelf="flex-end">
-          <Share title={props.data.title} />
+        <Box justifySelf="flex-end" alignSelf="start">
+          <Share title={props.data?.title} />
         </Box>
       )}
       contentTitleD="About"
+      renderContentB={() =>
+        props.data?.author?.firstName && (
+          <Box display="flex" mt="base">
+            <Box mr="base">
+              <Avatar
+                name={props.data?.author?.firstName}
+                source={props.data?.author?.photo?.uri}
+                height="60px"
+                width="60px"
+              />
+            </Box>
+            <Box display="flex" justifyContent="center" flexDirection="column">
+              <Box
+                as="h4"
+                mb="0"
+              >{`${props.data?.author?.firstName} ${props.data?.author?.lastName}`}</Box>
+              <Box>
+                {format(new Date(props.data?.publishDate), 'MMM d, yyyy')}
+              </Box>
+            </Box>
+          </Box>
+        )
+      }
       htmlContent={props.data.htmlContent}
     />
   );
