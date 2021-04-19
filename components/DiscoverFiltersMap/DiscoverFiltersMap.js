9692a4d2be8dce1289cc7806bdac7c43916e38f7
Jordan Wade <jjwade31@gmail.com>
2021-04-19 16:12:15 -0500

    Remove extra loading component on discover

--- components/DiscoverFiltersMap/DiscoverFiltersMap.js
+++ components/DiscoverFiltersMap/DiscoverFiltersMap.js
@@ -1,11 +1,8 @@
 import React from 'react';
 
-import { Loader } from 'ui-kit';
 import { DiscoverFilterSection } from 'components';
 
 const DiscoverFiltersMap = props => {
-  if (props.loading) return <Loader justifyContent="center" />;
-
   return props.data
     .map(edge => edge.node)
     .map((node, i) => (
