9627503317bf68b1f1ba11d47e258468a755b929
Drew Barontini <hello@drewb.io>
2021-04-20 16:21:32 -0400

    Handle undefined id in currentUserIsLeader

--- utils/currentUserIsLeader.js
+++ utils/currentUserIsLeader.js
@@ -1,7 +1,7 @@
 import find from 'lodash/find';
 
 function currentUserIsLeader(currentUser, leaders) {
-  const id = _id => _id.split(':').pop();
+  const id = _id => (!_id ? _id : _id.split(':').pop());
   const match = find(
     leaders,
     leader => id(leader?.node?.id) === id(currentUser?.id)
