75c687a77724c1c35ef57fd85de872fcf354c63d
Jordan Wade <jjwade31@gmail.com>
2021-04-21 16:44:05 -0500

    Move 'Cell' out of 'Layout' to improve per page theming

--- pages/profile.js
+++ pages/profile.js
@@ -1,10 +1,18 @@
 import { Layout, UserProfile } from 'components';
 import { CurrentPersonProvider } from 'providers';
+import { Cell, utils } from 'ui-kit';
 
 export default function Profile(props = {}) {
   return (
     <Layout title="Profile">
-      <CurrentPersonProvider Component={UserProfile} />
+      <Cell
+        as="main"
+        maxWidth={utils.rem('1100px')}
+        px="base"
+        py={{ _: 'l', lg: 'xl' }}
+      >
+        <CurrentPersonProvider Component={UserProfile} />
+      </Cell>
     </Layout>
   );
 }
