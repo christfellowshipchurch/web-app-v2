e48105ba526ee1f1188731aeb05c2d94e085ed0e
Michael Neeley <micneeley14@gmail.com>
2021-04-23 15:53:26 -0400

    removed search header text

--- pages/search.js
+++ pages/search.js
@@ -1,7 +1,7 @@
 import { Layout } from 'components';
 import { Search } from 'components';
 import { useState } from 'react';
-import { CardGrid, Heading, Text } from 'ui-kit';
+import { CardGrid } from 'ui-kit';
 
 export function SearchPageContent({ filtering, setFiltering }) {
   return (
@@ -13,26 +13,6 @@ export function SearchPageContent({ filtering, setFiltering }) {
       width="100%"
       bg="bg"
     >
-      <Heading
-        color="neutrals.900"
-        variant="h2"
-        fontWeight="800"
-        textAlign="center"
-      >
-        Need to find something?
-      </Heading>
-      <Text
-        variant="s"
-        color="neutrals.900"
-        opacity="60%"
-        fontWeight="400"
-        mb="xl"
-        maxWidth="700px"
-        textAlign="center"
-      >
-        Search for sermons and events. Filter by categories such as campus or
-        schedule. You can even search for staff!
-      </Text>
       <Search filtering={filtering} setFiltering={setFiltering} />
     </CardGrid>
   );
