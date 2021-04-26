6da029939b8c16bda29bae4e6f58b55546098882
Ryan Davidson <ryanrkd2112@gmail.com>
2021-04-26 15:07:47 -0400

    Login routes stubbed

---
+++ pages/login/forgot/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Layout } from 'components';
+
+export default function ForgotPassword(props) {
+  return (
+    <Layout title="Forgot Password">
+      <h1>Forgot Password</h1>
+    </Layout>
+  );
+}
---
+++ pages/login/reset-password/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Layout } from 'components';
+
+export default function ResetPassword(props) {
+  return (
+    <Layout title="Reset Password">
+      <h1>Reset Password</h1>
+    </Layout>
+  );
+}
