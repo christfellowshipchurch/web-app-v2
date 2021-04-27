dc1000076fc8d12e3ab5807e63db78b963adde37
Luke Mommaerts <luke@differential.com>
2021-04-27 11:38:52 -0400

    Remove circle loader, change progress bar color

--- config/nprogress.js
+++ config/nprogress.js
@@ -2,6 +2,8 @@ import Router from 'next/router';
 import NProgress from 'nprogress';
 
 function configureNProgress() {
+  NProgress.configure({ showSpinner: false });
+
   Router.onRouteChangeStart = () => NProgress.start();
   Router.onRouteChangeComplete = () => NProgress.done();
   Router.onRouteChangeError = () => NProgress.done();
