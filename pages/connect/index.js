468fa8f88766dc86ed03c31d97b4837b7f400d68
Ryan Davidson <ryanrkd2112@gmail.com>
2021-04-28 14:56:40 -0400

    Prevent unauthenticated access to Connect

--- pages/connect/index.js
+++ pages/connect/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 import { FeatureFeedProvider } from 'providers';
+import { useAuth } from 'providers/AuthProvider';
+
 import { Layout, FeatureFeed } from 'components';
 import { Cell, utils } from 'ui-kit';
 
 export default function Connect(props = {}) {
+  const router = useRouter();
+  const [{ authenticated }] = useAuth();
   const options = {
     variables: {
       pathname: 'connect',
     },
   };
+
+  useEffect(() => {
+    if (!authenticated) {
+      router.push('/');
+    }
+  }, [router, authenticated]);
+
+  if (!authenticated) return null;
 
   return (
     <Layout title="Connect">
