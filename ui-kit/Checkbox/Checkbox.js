0d74e804c7059b101e1896f9acc7c972b949a51f
Maria Lisandra Alfonso <46769629+codingwithlis@users.noreply.github.com>
2021-05-05 16:37:58 -0400

    Updated terms and conditions checkbox error message CFDP-1356

--- ui-kit/Checkbox/Checkbox.js
+++ ui-kit/Checkbox/Checkbox.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 
 import { systemPropTypes } from 'ui-kit';
 import Styled from './Checkbox.styles';
+import { useCustomValidity } from 'device-agnostic-ui';
 
 function Checkbox(props = {}) {
+  const ref = useRef();
+  useCustomValidity(ref, props.validationMessage);
+  
   return (
     <Styled {...props.containerProps}>
-      <Styled.Input id={props.id} name={props.id} {...props} />
+      <Styled.Input id={props.id} name={props.id} ref={ref} {...props} />
       {props.label && (
         <Styled.Label htmlFor={props.id} {...props.labelProps}>
           {props.label}
@@ -31,6 +35,7 @@ Checkbox.propTypes = {
 
 Checkbox.defaultProps = {
   type: 'checkbox',
+  validationMessage: "Select this checkbox to continue"
 };
 
 export default Checkbox;
