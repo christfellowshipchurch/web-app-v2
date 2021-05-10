c154efa5a9d821a1c260da701849d4e96f7ae3df
Jordan Wade <jjwade31@gmail.com>
2021-05-10 16:28:14 -0500

    Add group facet filter hook

---
+++ hooks/useGroupFacetFilters.js
@@ -0,0 +1,18 @@
+import { gql, useQuery } from '@apollo/client';
+
+export const GET_LINEUPS = gql`
+  query getGroupFacetFilters($facet: String!, $facetFilters: [String]) {
+    groupFacetFilters(facet: $facet, facetFilters: $facetFilters)
+  }
+`;
+
+function useGroupFacetFilters(options = {}) {
+  const query = useQuery(GET_LINEUPS, options);
+
+  return {
+    facets: query?.data?.groupFacetFilters || [],
+    ...query,
+  };
+}
+
+export default useGroupFacetFilters;
