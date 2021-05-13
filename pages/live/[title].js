754132ef7f0a6690f3878380f6dc21202e3ba726
Caleb Panza <caleb.panza@icloud.org>
2021-05-13 16:28:37 -0400

    Comment out Live Stream pre-render data

--- pages/live/[title].js
+++ pages/live/[title].js
@@ -27,45 +27,45 @@ LiveStream.propTypes = {
   liveStreamId: PropTypes.string.isRequired,
 };
 
-export async function getStaticProps(context) {
-  const apolloClient = initializeApollo();
-  const title = context.params.title;
+// export async function getStaticProps(context) {
+//   const apolloClient = initializeApollo();
+//   const title = context.params.title;
 
-  const query = await apolloClient.query({ query: GET_LIVE_STREAMS });
-  const liveStreams = query.data?.liveStreams || [];
+//   const query = await apolloClient.query({ query: GET_LIVE_STREAMS });
+//   const liveStreams = query.data?.liveStreams || [];
 
-  const matchedLiveStream = liveStreams.find(
-    liveStream => slugify(liveStream.relatedNode?.title) === title
-  );
+//   const matchedLiveStream = liveStreams.find(
+//     liveStream => slugify(liveStream.relatedNode?.title) === title
+//   );
 
-  if (matchedLiveStream) {
-    await apolloClient.query({
-      query: GET_LIVE_STREAM,
-      variables: {
-        id: matchedLiveStream.id,
-      },
-    });
-  }
+//   if (matchedLiveStream) {
+//     await apolloClient.query({
+//       query: GET_LIVE_STREAM,
+//       variables: {
+//         id: matchedLiveStream.id,
+//       },
+//     });
+//   }
 
-  return {
-    props: {
-      initialApolloState: apolloClient.cache.extract(),
-      liveStreamId: matchedLiveStream.id,
-      liveStreamTitle: matchedLiveStream.relatedNode?.title,
-    },
-    revalidate: 1,
-  };
-}
+//   return {
+//     props: {
+//       initialApolloState: apolloClient.cache.extract(),
+//       liveStreamId: matchedLiveStream.id,
+//       liveStreamTitle: matchedLiveStream.relatedNode?.title,
+//     },
+//     revalidate: 1,
+//   };
+// }
 
-export async function getStaticPaths() {
-  const apolloClient = initializeApollo();
+// export async function getStaticPaths() {
+//   const apolloClient = initializeApollo();
 
-  const query = await apolloClient.query({ query: GET_LIVE_STREAMS });
-  const liveStreams = query.data?.liveStreams || [];
+//   const query = await apolloClient.query({ query: GET_LIVE_STREAMS });
+//   const liveStreams = query.data?.liveStreams || [];
 
-  const paths = liveStreams.map(liveStream => ({
-    params: { title: slugify(liveStream.relatedNode.title) },
-  }));
+//   const paths = liveStreams.map(liveStream => ({
+//     params: { title: slugify(liveStream.relatedNode.title) },
+//   }));
 
-  return { paths, fallback: false };
-}
+//   return { paths, fallback: false };
+// }
