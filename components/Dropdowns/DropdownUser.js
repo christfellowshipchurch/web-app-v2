c987e4fd20ab714426da2d93f7a8d792aed96c0a
Vincent Wilson <vincent@differential.com>
2021-05-28 09:21:28 -0400

    Setup new profile links

--- components/Dropdowns/DropdownUser.js
+++ components/Dropdowns/DropdownUser.js
@@ -5,26 +5,28 @@ import { Box, Heading } from 'ui-kit';
 import { useRouter } from 'next/router';
 import { showModal, useModalDispatch } from 'providers/ModalProvider';
 
-function Profile() {
+
+export default function DropdownUser() {
   const router = useRouter();
+  const { currentPerson, authenticated } = useCurrentPerson();
+  const modalDispatch = useModalDispatch();
   const dispatch = useAuthDispatch();
-  const { currentPerson } = useCurrentPerson();
   return (
     <Box
+      bg="bg"
+      p="s"
+      alignItems="flex-start"
+      boxShadow="base"
       display="flex"
       flexDirection="column"
-      alignItems="flex-start"
-      width="100%"
-      borderTop="1px solid grey"
-      pt="xxs"
     >
-      <Heading width="100%" p="0.875rem">
+      { authenticated && <Heading width="100%" p="0.875rem">
         {[
           currentPerson.profile?.firstName,
           currentPerson.profile?.lastName,
         ].join(' ')}
-      </Heading>
-      <Button
+      </Heading>}
+      { authenticated && <Button
         borderRadius="base"
         width="100%"
         onClick={() => {
@@ -32,7 +34,7 @@ function Profile() {
         }}
       >
         Profile
-      </Button>
+      </Button>}
       <Button
         borderRadius="base"
         width="100%"
@@ -69,90 +71,19 @@ function Profile() {
       >
         Communication Preferences
       </Button>
-      <Button
+      {authenticated ?       <Button
         borderRadius="base"
         width="100%"
         onClick={() => dispatch(logout())}
       >
         Log Out
-      </Button>
-    </Box>
-  );
-}
-
-function Login() {
-  const modalDispatch = useModalDispatch();
-
-  return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      alignItems="flex-start"
-      width="100%"
-      borderTop="1px solid grey"
-      pt="xxs"
-    >
-      <Button
+      </Button> :       <Button
         borderRadius="base"
         width="100%"
         onClick={() => modalDispatch(showModal('Auth'))}
       >
         Sign In
-      </Button>
-    </Box>
-  );
-}
-
-export default function DropdownUser() {
-  const router = useRouter();
-  const { authenticated } = useCurrentPerson();
-  return (
-    <Box
-      bg="bg"
-      p="s"
-      alignItems="flex-start"
-      boxShadow="base"
-      display="flex"
-      flexDirection="column"
-    >
-      <Button
-        width="100%"
-        borderRadius="base"
-        onClick={() => router.push('/give')}
-      >
-        Give
-      </Button>
-      <Button
-        width="100%"
-        borderRadius="base"
-        onClick={() => router.push('/groups')}
-      >
-        Groups
-      </Button>
-      <Button
-        width="100%"
-        borderRadius="base"
-        onClick={() => router.push('/events')}
-      >
-        Events
-      </Button>
-      <Button
-        width="100%"
-        borderRadius="base"
-        onClick={() => router.push('/serve')}
-      >
-        Serve
-      </Button>
-      <Button
-        borderRadius="base"
-        width="100%"
-        onClick={() => {
-          router.push('/connect-with-us');
-        }}
-      >
-        Talk to Someone
-      </Button>
-      {authenticated ? <Profile /> : <Login />}
+      </Button>}
     </Box>
   );
 }
