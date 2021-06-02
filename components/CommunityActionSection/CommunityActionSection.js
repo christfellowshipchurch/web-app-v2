c3ae88ba0ed183a0b1fd11ce6037ad3624620370
dzwood <46049974+dzwood@users.noreply.github.com>
2021-06-02 16:02:01 -0400

    Updated CommunityActionSection to accept titles for 'Search All' button. (#249)

--- components/CommunityActionSection/CommunityActionSection.js
+++ components/CommunityActionSection/CommunityActionSection.js
@@ -19,7 +19,8 @@ export default function CommunityActionSection(props) {
         that’s right for you!
       </Box>
       <Button onClick={props.handleOnClick} rounded={true} mb="base">
-        Search ALL Groups &amp; Classes
+        {/* Allows for Group Hub titles to be passed in */}
+        {`Search All ${props?.title ? props?.title + ' ' : ''}Groups & Classes`}
       </Button>
 
       <CustomLink target="_blank" href="https://rock.gocf.org/page/2113">
