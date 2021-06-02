7c008c889b8a67196bd61f422a92755db5a8cb20
asleepingpanda <45076058+asleepingpanda@users.noreply.github.com>
2021-06-02 16:04:21 -0400

    Enable My Groups messaging (#250)
    * My Groups underneath the main summary
    * My Groups on the bottom fold
    
    Co-authored-by: Caleb Panza <caleb.panza@icloud.org>
    Co-authored-by: dzwood <46049974+dzwood@users.noreply.github.com>

--- components/CommunityLeaderActions/CommunityLeaderActions.js
+++ components/CommunityLeaderActions/CommunityLeaderActions.js
@@ -1,16 +1,60 @@
 import { Box, Button, Icon } from 'ui-kit';
+import { useRouter } from 'next/router';
+import { update as updateAuth, useAuth } from 'providers/AuthProvider';
+import { useModalDispatch, showModal } from 'providers/ModalProvider';
 
 export default function CommunityLeaderActions(props) {
+  const router = useRouter();
+  const [{ authenticated }, authDispatch] = useAuth();
+  const modalDispatch = useModalDispatch();
+
+  function ensureAuthentication(onSuccess) {
+    if (!authenticated) {
+      modalDispatch(showModal('Auth'));
+      authDispatch(updateAuth({ onSuccess }));
+    } else {
+      onSuccess();
+    }
+  }
+
+  function handleMyGroups() {
+    const navigateToConnect = () => {
+      router.push('/connect');
+    };
+
+    ensureAuthentication(navigateToConnect);
+  }
+
   return (
     <Box backgroundColor="white">
       <Box
         display="grid"
         px={{ _: 'base', md: 'xxl' }}
         py={{ _: 'l', md: 'xxl' }}
-        gridTemplateColumns={{ _: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }}
+        gridTemplateColumns={{ _: 'repeat(1, 1fr)', md: 'repeat(3, 1fr)' }}
         gridColumnGap="l"
         textAlign="center"
       >
+        <Box mb={{ _: 'xl', md: 0 }}>
+          <Icon name="user" size="40" mb="s" color="subdued" />
+          <Box>
+            <Box as="h2" mb="s">
+              My Groups
+            </Box>
+            <Box as="p" mb="base">
+              Already in a group? Log in to view your groups.
+            </Box>
+            <Button
+              size="s"
+              variant="secondary"
+              rounded={true}
+              onClick={handleMyGroups}
+            >
+              View My Groups
+            </Button>
+          </Box>
+        </Box>
+
         <Box mb={{ _: 'xl', md: 0 }}>
           <Icon name="users" size="40" mb="s" color="subdued" />
           <Box>
@@ -31,6 +75,7 @@ export default function CommunityLeaderActions(props) {
             </Button>
           </Box>
         </Box>
+
         <Box mb={{ _: 'xl', md: 0 }}>
           <Icon name="book" size="40" mb="s" color="subdued" />
           <Box>
