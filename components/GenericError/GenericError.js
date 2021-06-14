e689ed19f6927c97b81be9570ea94ba8d0012516
Ryan Davidson <ryanrkd2112@gmail.com>
2021-06-14 10:48:23 -0400

    Create GenericError component

---
+++ components/GenericError/GenericError.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { Box } from 'ui-kit';
+import { Layout } from 'components';
+
+const GenericError = () => {
+  return (
+    <Layout title="Error">
+      <Box mt="xl" textAlign="center">
+        <Box
+          as="img"
+          alt="Error Image"
+          width="90%%"
+          maxWidth={750}
+          src="/error.png"
+          margin="auto"
+        />
+        <Box as="h1" mt="l">
+          Oops! Something went wrong.
+        </Box>
+        <Box as="p" fontSize="l" mb="xxl">
+          We were unable to load the content you were looking for. Please hit
+          back and try again.
+        </Box>
+      </Box>
+    </Layout>
+  );
+};
+
+export default GenericError;
