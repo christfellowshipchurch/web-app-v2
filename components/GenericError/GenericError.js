add9123952a37f681e4874d9bef47a504b951f1e
Ryan Davidson <rkdavidson@users.noreply.github.com>
2021-06-14 12:08:36 -0400

    Generic error handler (#276)
    * Create GenericError component
    
    * Create /error page and redirect from Home on timeout
    
    * Language nitpick on error
    
    * Link to contact form on error
    
    Co-authored-by: asleepingpanda <45076058+asleepingpanda@users.noreply.github.com>

---
+++ components/GenericError/GenericError.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { Box } from 'ui-kit';
+import { CustomLink, Layout } from 'components';
+
+const GenericError = () => {
+  return (
+    <Layout title="Error">
+      <Box mt="xl" textAlign="center">
+        <Box
+          as="img"
+          alt="Error Image"
+          width="90%%"
+          maxWidth={750}
+          src="/error.png"
+          margin="auto"
+        />
+        <Box as="h1" mt="l">
+          Oops! Something went wrong.
+        </Box>
+        <Box as="p" fontSize="l">
+          We were unable to load the content you were looking for. Please go
+          back and try again.
+        </Box>
+        <Box as="p" fontSize="l" mb="xxl">
+          If you're still having trouble, you can{' '}
+          <CustomLink href="https://rock.gocf.org/contactus" title="Contact Us">
+            contact us
+          </CustomLink>{' '}
+          for assistance.
+        </Box>
+      </Box>
+    </Layout>
+  );
+};
+
+export default GenericError;
