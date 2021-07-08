dcce64a7c1de9b4a3125d072d3dfa8edb2bf8816
Luke Mommaerts <luke@differential.com>
2021-07-08 12:45:26 -0400

    Fix articles on home page

--- components/HomeFeed/HomeFeed.js
+++ components/HomeFeed/HomeFeed.js
@@ -275,7 +275,7 @@ function HomeFeedContent(props = {}) {
   const largeArticle = articles.find(article => article?.featureOnHomePage);
   const miniArticles = articles
     .filter(article => !article?.featureOnHomePage && article?.showOnHomePage)
-    .slice(1, 3);
+    .slice(0, 2);
 
   // Fixes a very strange static generation error I was running into.
   // In effect - when this page was rendered for an authed user
