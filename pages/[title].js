5fbfdd8a57310281f7f4d3720c80821892392039
Maria Lisandra Alfonso <46769629+codingwithlis@users.noreply.github.com>
2021-07-14 12:54:13 -0400

    Added getStaticPaths and getStaticProps to [title].js

--- pages/[title].js
+++ pages/[title].js
@@ -5,6 +5,7 @@ import { kebabCase, toLower, camelCase, capitalize } from 'lodash';
 import { initializeApollo } from 'lib/apolloClient';
 import { GET_FEATURE_FEED } from 'hooks/useFeatureFeed';
 import { GET_FEATURE } from 'hooks/useFeature';
+import { GET_CONTENT_ITEM } from 'hooks/useContentItem';
 import { FeatureFeedProvider, ContentItemProvider } from 'providers';
 import { Layout, FeatureFeed, ContentSingle, PageSingle } from 'components';
 
@@ -29,6 +30,39 @@ export default function PageBuilder(props = {}) {
       <FeatureFeedProvider Component={FeatureFeed} options={options} />
     </Layout>
   );
+}
+
+// This function gets called at build time to generate the titles for _all_ messages
+export async function getStaticPaths() {
+  // todo : make this a Network request so that it's dynamic
+  const titles = [];
+
+  return {
+    paths: titles.map(title => title),
+    // Enable statically generating additional pages
+    // For example: `/messages/another-great-sermon`
+    fallback: true,
+  };
+}
+
+// This also gets called at build time
+export async function getStaticProps({ params }) {
+  const apolloClient = initializeApollo();
+
+  await apolloClient.query({
+    query: GET_CONTENT_ITEM,
+    variables: { pathname: params.title },
+  });
+
+  // Pass post data to the page via props
+  return {
+    props: {
+      initialApolloState: apolloClient.cache.extract(),
+    },
+    // Re-generate the post at most once per second
+    // if a request comes in
+    revalidate: 1,
+  };
 }
 
 // export async function getServerSideProps() {
