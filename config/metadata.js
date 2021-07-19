d6b158ddc36cb8355577d3fc7ee0dd38fc548ba5
Maria Lisandra Alfonso <46769629+codingwithlis@users.noreply.github.com>
2021-07-19 15:02:31 -0400

    added-subscribe-to-updates-link

--- config/metadata.js
+++ config/metadata.js
@@ -1,15 +1,13 @@
 const metadata = {
   links: {
     churchOnline: 'https://www.youtube.com/c/ChristFellowshipWelcomeHome',
-    pastMessages:
-      'https://www.youtube.com/playlist?list=PLUQ7jSnRB_efXMDq9Lka6stS02awWoaz4',
+    pastMessages: 'https://www.youtube.com/playlist?list=PLUQ7jSnRB_efXMDq9Lka6stS02awWoaz4',
     giveOnline: 'https://pushpay.com/g/christfellowship',
     volunteer: 'https://rock.christfellowship.church/dreamteam',
     shopOnline: 'https://resource.gochristfellowship.com/',
     connectCard: 'https://rock.gocf.org/connect',
     submitPrayerRequest: 'https://rock.gocf.org/RequestPrayer',
-    subscribeToUpdates:
-      'https://gochristfellowship.us11.list-manage.com/subscribe?u=76848e191018191e2e2d01d77&id=528c3363d4',
+    subscribeToUpdates:'http://eepurl.com/hAk7aP',
     contactUs: 'https://rock.gocf.org/contactus',
     careerOpportunities: 'https://boards.greenhouse.io/christfellowship',
     facebook: 'https://www.facebook.com/CFimpact/',
