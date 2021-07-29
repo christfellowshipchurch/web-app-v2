11a5f769440b685ae8c2c19423515cd3bb230672
Conrad VanLandingham <conrad@differential.com>
2021-07-29 15:47:51 -0500

    add gtag

--- components/SEO/SEO.js
+++ components/SEO/SEO.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import gtag from '../../lib/gtag';
 
 const DEFAULT_DESCRIPTION = `We exist to invite one another into a growing relationship with Jesus.`;
 const DEFAULT_KEYWORDS = `Church, Long Hollow, Long Hollow Baptist, Long Hollow Church, Churches in Nashville`;
@@ -17,6 +18,53 @@ function SEO(props = {}) {
   const router = useRouter();
   const title = getPageTitle(props.meta.title || props.title);
   const url = `https://longhollow.com${router.asPath}`;
+
+  useEffect(() => {
+    let gtagValid = true;
+
+    // Do not run Google Analytics unless traffic is coming from a browser
+    const _isNotBrowser =
+      typeof window === 'undefined' || typeof document === 'undefined';
+
+    if (_isNotBrowser) return null;
+
+    // Only run Google Analytics
+    if (process.env.NODE_ENV !== 'production') return null;
+
+    // NEXT_PUBLIC_GA_CODE  needs to be set in the .env
+    if (!process.env.NEXT_PUBLIC_GA_CODE) {
+      console.warn(
+        'GoogleAnalytics tracking code is required to initialize GoogleAnalytics'
+      );
+
+      gtagValid = false;
+    }
+
+    if (!window.gtag) {
+      console.warn(
+        'GoogleAnalytics should be loaded manually before gtag is called.'
+      );
+
+      gtagValid = false;
+    }
+
+
+    const handleRouteChange = url => {
+      if (gtagValid) {
+        gtag.pageview(url);
+      }
+    };
+
+    if (gtagValid) {
+      gtag.pageview();
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <Head>
@@ -36,6 +84,31 @@ function SEO(props = {}) {
         name="viewport"
         content="width=device-width, initial-scale=1, shrink-to-fit=no"
       />
+      {/* Global site tag (gtag.js) - Google Analytics */}
+      {process.env.NODE_ENV === 'production' &&
+      process.env.NEXT_PUBLIC_GA_CODE ? (
+        <>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_CODE}`}
+          />
+          <script
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${process.env.NEXT_PUBLIC_GA_CODE}', {
+      page_path: window.location.pathname,
+    });
+  `,
+            }}
+          />
+        </>
+      ) : null}
+
+
       {props.children}
     </Head>
   );
