e90299a27e8faf86f040c79d074675ccea81ebfd
asleepingpanda <45076058+asleepingpanda@users.noreply.github.com>
2021-08-03 11:56:52 -0400

    Support for different Group Actions in the Group Finder (#317)
    * New Group Search Fragment
    
    * Removes querying for a Group to get visual details for Group Card
    * Introduces the new `GroupSearchResult` type in the query
    * Sets the fetch to `network-only`
    
    TODO
    * There still seems to be a cache consideration taking place somewhere and we do NOT want search results cached
    
    * Update Visual Components
    
    * GroupCard's visual elements are fully derived from the `group` object, not the `group.node` object
    * The "Contact" button will change to "Register" when there is a `Url` node present
    * When a `Url` is passed down in the `node`, we'll go ahead and open up a new tab when pressing "Register". All other interactions will still open up the "Connect" modal
    
    * Introduce the useFeatureAction hook
    
    * Adds support for CONTACT_GROUP action
    * Centralized all Feature Action logic
    * no-cache policy on the Group Search to ping server for ALL Group Searches
    
    Co-authored-by: Caleb Panza <caleb.panza@icloud.org>
    Co-authored-by: dzwood <46049974+dzwood@users.noreply.github.com>

--- hooks/useSearchGroups.js
+++ hooks/useSearchGroups.js
@@ -9,6 +9,7 @@ export const SEARCH_GROUPS = gql`
         endCursor
       }
       edges {
+        id
         cursor
         title
         summary
@@ -17,43 +18,30 @@ export const SEARCH_GROUPS = gql`
             uri
           }
         }
-        node {
+        relatedNode {
           id
-          ...groupSearchResultNodeFragment
+
+          ... on Url {
+            url
+          }
         }
+        action
+        ...groupSearchResultNodeFragment
       }
     }
   }
 
-  fragment groupSearchResultNodeFragment on Node {
-    ... on Group {
-      preference
-      subPreference
-      meetingType
-      campus {
-        name
-      }
-      dateTime {
-        start
-      }
-    }
-    ... on GroupItem {
-      members: people(first: 1) {
-        totalCount
-      }
-      leaders: people(first: 3, isLeader: true) {
-        totalCount
-        edges {
-          node {
-            id
-            firstName
-            lastName
-            nickName
-            photo {
-              uri
-            }
-          }
-        }
+  fragment groupSearchResultNodeFragment on GroupSearchResult {
+    preferences
+    subPreferences
+    meetingDay
+    meetingType
+    campusName
+    leaders {
+      firstName
+      lastName
+      photo {
+        uri
       }
     }
   }
@@ -61,8 +49,8 @@ export const SEARCH_GROUPS = gql`
 
 function useSearchGroups(options = {}) {
   const [searchGroups, query] = useLazyQuery(SEARCH_GROUPS, {
-    fetchPolicy: 'cache-and-network',
     ...options,
+    fetchPolicy: 'no-cache',
   });
 
   return [
