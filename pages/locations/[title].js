fb451ac65fc8a391ce7c79a1a29158d87335f65d
Maria Lisandra Alfonso <46769629+codingwithlis@users.noreply.github.com>
2021-08-04 12:35:36 -0400

    Copied the pre-rendering methods to articles, locations, and studies

--- pages/locations/[title].js
+++ pages/locations/[title].js
@@ -4,6 +4,9 @@ import { useRouter } from 'next/router';
 import { ContentItemProvider } from 'providers';
 import { LocationSingle } from 'components';
 
+import { GET_CONTENT_ITEM } from 'hooks/useContentItem';
+import { initializeApollo } from 'lib/apolloClient';
+
 export default function Location(props = {}) {
   const router = useRouter();
   const { query } = router;
@@ -17,3 +20,37 @@ export default function Location(props = {}) {
 
   return <ContentItemProvider Component={LocationSingle} options={options} />;
 }
+
+// This function gets called at build time to generate the titles for _all_ messages
+export async function getStaticPaths() {
+  // todo : make this a Network request so that it's dynamic
+  const titles = [];
+
+  return {
+    paths: titles.map(title => `locations/${title}`),
+    // Enable statically generating additional pages
+    // For example: `/messages/another-great-sermon`
+    fallback: true,
+  };
+}
+
+// This also gets called at build time
+export async function getStaticProps({ params }) {
+  const apolloClient = initializeApollo();
+
+  await apolloClient.query({
+    query: GET_CONTENT_ITEM,
+    variables: { pathname: `locations/${params.title}` },
+  });
+
+  // Pass post data to the page via props
+  return {
+    props: {
+      initialApolloState: apolloClient.cache.extract(),
+    },
+    // Re-generate the post at most once per second
+    // if a request comes in
+    revalidate: 1,
+  };
+}
+
--- pages/studies/[title].js
+++ pages/studies/[title].js
@@ -2,6 +2,9 @@ import { useRouter } from 'next/router';
 import { ContentItemProvider } from 'providers';
 import { ContentSingle, Layout } from 'components';
 
+import { GET_CONTENT_ITEM } from 'hooks/useContentItem';
+import { initializeApollo } from 'lib/apolloClient';
+
 function getItemId(slug) {
   const id = slug.split('-').pop();
   return `MediaContentItem:${id}`;
@@ -22,6 +25,39 @@ export default function Study(props) {
       <ContentItemProvider Component={ContentSingle} options={options} />
     </Layout>
   );
+}
+
+// This function gets called at build time to generate the titles for _all_ messages
+export async function getStaticPaths() {
+  // todo : make this a Network request so that it's dynamic
+  const titles = [];
+
+  return {
+    paths: titles.map(title => `locations/${title}`),
+    // Enable statically generating additional pages
+    // For example: `/messages/another-great-sermon`
+    fallback: true,
+  };
+}
+
+// This also gets called at build time
+export async function getStaticProps({ params }) {
+  const apolloClient = initializeApollo();
+
+  await apolloClient.query({
+    query: GET_CONTENT_ITEM,
+    variables: { pathname: `locations/${params.title}` },
+  });
+
+  // Pass post data to the page via props
+  return {
+    props: {
+      initialApolloState: apolloClient.cache.extract(),
+    },
+    // Re-generate the post at most once per second
+    // if a request comes in
+    revalidate: 1,
+  };
 }
 
 /**
