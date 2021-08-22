d82e1a62802b4b769a54728835fbfc2a409ce6f8
ericmurrell <ericmurrell@gmail.com>
2021-08-21 20:17:34 -0500

    sermon notes

--- next.config.js
+++ next.config.js
@@ -222,7 +222,7 @@ module.exports = {
       { source: '/events/lh-kids-baptism-classes', destination: '/kids-baptism-classes', permanent: true },
       { source: '/events/supporting-those-who-struggle-event', destination: '/supporting-those-who-struggle-event', permanent: true },
       { source: '/events/light-up-the-pike', destination: 'https://lightupthepike.com', permanent: true },
-      { source: '/notes', destination: 'http://bible.com/events/48746366', permanent: true }
+      { source: '/notes', destination: 'http://bible.com/events/48749043', permanent: true }
     ];
   },
 };
