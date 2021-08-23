1a206eaa7ca927d7f29abe9a8a6d28ae7b369c7c
ericmurrell <ericmurrell@gmail.com>
2021-08-23 15:53:23 -0500

    servelink

--- next.config.js
+++ next.config.js
@@ -59,7 +59,7 @@ module.exports = {
       { source: '/session2', destination: 'https://my.longhollow.com/startingpoint2', permanent: true },
       { source: '/session3', destination: 'https://my.longhollow.com/startingpoint3', permanent: true },
       { source: '/session4', destination: 'https://my.longhollow.com/startingpoint4', permanent: true },
-      { source: '/servelink', destination: '/next-steps/volunteer', permanent: true },
+      { source: '/servelink', destination: '/servelink-aug-29', permanent: true },
       { source: '/app', destination: '/the-long-hollow-app', permanent: true },
       { source: '/supportgroups', destination: '/connect/support-groups', permanent: true },
       { source: '/beliefs', destination: '/about/beliefs', permanent: true },
