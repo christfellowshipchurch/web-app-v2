81dd85a79abb4a785601508e6dc38c4db5299af6
jcallais <89946120+jcallais@users.noreply.github.com>
2021-09-07 15:39:14 -0500

    Update next.config.js
    /rhythms Short Link

--- next.config.js
+++ next.config.js
@@ -32,6 +32,7 @@ module.exports = {
         destination: '/:slug',
         permanent: false,
       }, //eric test again 
+      { source: '/rhythms', destination: '/rhythms-september-2021', permanent: true },
       { source: '/survey', destination: 'https://www.surveymonkey.com/r/YFZMCFP', permanent: true },
       { source: '/hvillemap', destination: 'https://rock.longhollow.com/Content/ExternalSite/Downloads/HENDERSONVILLE_MAP_COMBO.pdf', permanent: true },
       { source: '/mensnextsteps', destination: '/next-steps-from-mens-night-2021', permanent: true },
