cb69b22b3952d65ba9bf4a6f218653954969c4d3
dzwood <46049974+dzwood@users.noreply.github.com>
2021-10-13 14:23:56 -0400

    Added So Good Sister Hood URL

---
+++ pages/so-good-sisterhood/[title].js
@@ -0,0 +1,61 @@
+import { useRouter } from 'next/router';
+import { ContentItemProvider } from 'providers';
+import { ContentSingle, Layout } from 'components';
+
+import { GET_CONTENT_ITEM } from 'hooks/useContentItem';
+import { initializeApollo } from 'lib/apolloClient';
+
+function getItemId(slug) {
+  const id = slug.split('-').pop();
+  return `MediaContentItem:${id}`;
+}
+
+export default function Article(props) {
+  const router = useRouter();
+  const { title } = router.query;
+
+  const options = {
+    variables: {
+      pathname: `so-good-sisterhood/${title}`,
+    },
+  };
+
+  return (
+    <Layout title={title}>
+      <ContentItemProvider Component={ContentSingle} options={options} />
+    </Layout>
+  );
+}
+
+// This function gets called at build time to generate the titles for _all_ messages
+export async function getStaticPaths() {
+  // todo : make this a Network request so that it's dynamic
+  const titles = [];
+
+  return {
+    paths: titles.map(title => `so-good-sisterhood/${title}`),
+    // Enable statically generating additional pages
+    // For example: `/messages/another-great-sermon`
+    fallback: true,
+  };
+}
+
+// This also gets called at build time
+export async function getStaticProps({ params }) {
+  const apolloClient = initializeApollo();
+
+  await apolloClient.query({
+    query: GET_CONTENT_ITEM,
+    variables: { pathname: `so-good-sisterhood/${params.title}` },
+  });
+
+  // Pass post data to the page via props
+  return {
+    props: {
+      initialApolloState: apolloClient.cache.extract(),
+    },
+    // Re-generate the post at most once per second
+    // if a request comes in
+    revalidate: 1,
+  };
+}
