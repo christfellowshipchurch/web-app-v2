dbd46e012281c9915e70e7c96589dcc17fc8d40c
bchamberlain <92399176+baychamberlain@users.noreply.github.com>
2021-11-01 11:16:25 -0500

    Spirit of Christmas

--- next.config.js
+++ next.config.js
@@ -32,6 +32,7 @@ module.exports = {
         destination: '/:slug',
         permanent: false,
       }, 
+      { source: '/spirit', destination: '/spirit-of-christmas-2021', permanent: true },
       { source: '/growtogether', destination: '/grow-together-november-2021', permanent: true },
       { source: '/legacy', destination: '/the-legacy-park-project', permanent: true },
       { source: '/legacypark', destination: '/the-legacy-park-project', permanent: true },
