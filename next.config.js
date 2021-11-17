7f8a8ce80b93c13be11cee11e3ef2cebaa072c93
bchamberlain <92399176+baychamberlain@users.noreply.github.com>
2021-11-17 09:42:48 -0600

    First Responder Meal Deliveries

--- next.config.js
+++ next.config.js
@@ -32,6 +32,7 @@ module.exports = {
         destination: '/:slug',
         permanent: false,
       }, 
+      { source: '/feed', destination: '/first-responder-meal-deliveries', permanent: true },
       { source: '/spirit', destination: '/spirit-of-christmas-2021', permanent: true },
       { source: '/growtogether', destination: '/grow-together-november-2021', permanent: true },
       { source: '/legacy', destination: '/the-legacy-park-project', permanent: true },
