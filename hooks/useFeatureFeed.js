b604b6996d01bd62293f5d0dfc3b63f41fa0afca
Lis Alfonso <46769629+codingwithlis@users.noreply.github.com>
2021-11-24 10:36:47 -0500

    Implemented give page collection (#382)
    Co-authored-by: asleepingpanda <45076058+asleepingpanda@users.noreply.github.com>

--- hooks/useFeatureFeed.js
+++ hooks/useFeatureFeed.js
@@ -1,4 +1,6 @@
 import { gql, useQuery } from '@apollo/client';
+import { CustomLink } from 'components';
+import clone from 'lodash/clone';
 
 import {
   ACTION_BAR_FEATURE_FRAGMENT,
@@ -40,16 +42,56 @@ export const GET_FEATURE_FEED = gql`
   ${VERTICAL_CARD_LIST_FEATURE_FRAGMENT}
 `;
 
+const customGiveContentBlockCollection = {
+  __typename: "ContentBlockCollectionFeature",
+  title: "What You're Giving To:",
+  cards: [
+    {
+      title: 'Everyone',
+      summary:
+        'What started as 40 people in a living room is now thousands gathering across the region and online through Christ Fellowship Everywhere. When you faithfully give toward tithes & offerings, you’re a part of reaching even more people with the love and message of Jesus Christ.',
+      coverImage: {sources: [{uri: '/give1.png'}]},
+      highlightWidth: '100%',
+      highlightWidthSmall: '10%',
+    },
+    {
+      title: 'Everyday',
+      summary:
+        'Our vision is to lead a radical transformation in our region and beyond. Your faithful giving throughout the year is what equips us to do just that through the ongoing operations of the church—including our ministries, buildings, and platform of digital resources.',
+      coverImage: {sources: [{uri: '/give2.png'}]},
+      highlightWidth: '100%',
+      highlightWidthSmall: '10%',
+    },
+    {
+      title: 'Everywhere',
+      summary:
+        'Through our regional and global missions partners, we’re positioned to meet needs as soon as they arise. Whether through regular tithes & offerings or through Christ Birthday Offering in December, every dollar you give makes a difference for those in need.',
+      coverImage: {sources: [{uri: '/give3.png'}]},
+      highlightWidth: '100%',
+      highlightWidthSmall: '10%',
+    }
+  ]
+}
+
 function useFeatureFeed(options = {}) {
   const query = useQuery(GET_FEATURE_FEED, {
     fetchPolicy: 'cache-and-network',
     ...options,
   });
 
+  const isGivePage = options?.variables?.pathname === "give"
+  let features = Array.isArray(query?.data?.featuresFeed?.features) ? clone(query?.data?.featuresFeed?.features) : []
+
+  if(isGivePage && features.length > 1){
+    features[1] = customGiveContentBlockCollection;
+    console.log(features)
+  }
+
   return {
-    features: query?.data?.featuresFeed?.features || [],
+    features: features,
     ...query,
   };
 }
 
 export default useFeatureFeed;
+
