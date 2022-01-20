6907f7cad89123b77c526ce38dadfb5391db6c21
Lis Alfonso <46769629+codingwithlis@users.noreply.github.com>
2022-01-20 10:47:11 -0500

    testing jasonLD (#420)

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { AppHead } from 'components';
+import JsonLD from 'components/JsonLD'
 
 import configureNProgress from 'config/nprogress';
 import { AppProvider } from 'providers';
@@ -11,6 +12,59 @@ import 'react-quill/dist/quill.snow.css'
 
 // Tracks the route changes and adds a bar to the top.
 configureNProgress();
+
+const christFellowship = {
+  "@type": "Organization",
+  "name": "Christ Fellowship Church",
+  "address": {
+    "@type": "PostalAddress",
+    "addressLocality": "Palm Beach Gandens",
+    "addressRegion": "FL",
+    "postalCode": "33418",
+    "streetAddress": "5343 Northlake Blvd"
+  },
+  "founder": [
+    {
+      "@type": "Person",
+      "name": "Tom Mullins",
+      "givenName": "Tom",
+      "familyName": "Mullins",
+      "url": "https://www.christfellowship.church/about",
+    },
+    {
+      "@type": "Person",
+      "name": "Donna Mullins",
+      "givenName": "Donna",
+      "familyName": "Mullins",
+      "url": "https://www.christfellowship.church/about",
+    },
+  ],
+  "telephone": "(561) 799-7600",
+  "url": "christfellowship.church",
+  "logo": "https://www.christfellowship.church/logo.png"
+}
+
+const todd = {
+  "@type": "Person",
+  "name": "Todd Mullins",
+  "givenName": "Todd",
+  "familyName": "Mullins",
+  "jobTitle": "Senior Pastor",
+  "worksFor": christFellowship,
+  "url": "https://christfellowship.church/about",
+  "image": "https://rock.christfellowship.church/GetImage.ashx?id=2343360&format=jpg"
+}
+
+const julie = {
+  "@type": "Person",
+  "name": "Julie Mullins",
+  "givenName": "Julie",
+  "familyName": "Mullins",
+  "jobTitle": "Senior Pastor",
+  "worksFor": christFellowship,
+  "url": "https://www.christfellowship.church/about",
+  "image": "https://rock.christfellowship.church/GetImage.ashx?id=2343358&format=jpg"
+}
 
 function App({ Component, pageProps }) {
   /**
@@ -27,6 +81,19 @@ function App({ Component, pageProps }) {
     <AppProvider initialApolloState={pageProps.initialApolloState}>
       <>
         <AppHead />
+        
+        <JsonLD schema={{
+            ...todd,
+            spouse: julie
+          }}
+        />
+        <JsonLD schema={{
+            ...julie,
+            spouse: todd
+          }}
+        />
+        <JsonLD schema={christFellowship}/>
+
         <Component {...pageProps} />
       </>
     </AppProvider>
