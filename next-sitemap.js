c00cdea6074573017265a388c256f63a1b510a44
dzwood <46049974+dzwood@users.noreply.github.com>
2022-02-24 15:00:54 -0500

    site map

---
+++ next-sitemap.js
@@ -0,0 +1,17 @@
+module.exports = {
+  siteUrl: 'https://www.christfellowship.church',
+  generateRobotsTxt: true,
+  exclude: [
+    '/group',
+    '/group/*',
+    '/api',
+    '/api/*',
+    '/profile',
+    '/external-home',
+    '/about/About.styles',
+    '/groups/Community.styles',
+    '/live',
+    '/groups/search/*',
+    '/connect',
+  ],
+};
