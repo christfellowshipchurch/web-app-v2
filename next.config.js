85148c0144e74ca987913038bf71ae117e32a685
bchamberlain <92399176+baychamberlain@users.noreply.github.com>
2022-03-08 11:03:19 -0600

    Serve Link

--- next.config.js
+++ next.config.js
@@ -82,7 +82,7 @@ module.exports = {
       { source: '/session2', destination: 'https://my.longhollow.com/startingpoint2', permanent: true },
       { source: '/session3', destination: 'https://my.longhollow.com/startingpoint3', permanent: true },
       { source: '/session4', destination: 'https://my.longhollow.com/startingpoint4', permanent: true },
-      { source: '/servelink', destination: '/serve-link-october-2021', permanent: true },
+      { source: '/servelink', destination: '/sign-up-for-serve-link', permanent: true },
       { source: '/app', destination: '/the-long-hollow-app', permanent: true },
       { source: '/supportgroups', destination: '/connect/support-groups', permanent: true },
       { source: '/beliefs', destination: '/about/beliefs', permanent: true },
