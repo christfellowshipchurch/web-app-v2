0aed617199eca8d5d74b3552f3b729f4d6ebd160
ericmurrell <ericmurrell@gmail.com>
2022-03-09 13:38:18 -0600

    Kids Check In page

--- next.config.js
+++ next.config.js
@@ -32,6 +32,7 @@ module.exports = {
         destination: '/:slug',
         permanent: false,
       }, //fixing mvmnt
+      { source: '/checkin', destination: '/how-to-check-in-for-kids-activities', permanent: true },
       { source: '/calling', destination: '/discover-your-calling', permanent: true },
       { source: '/camp', destination: 'https://longhollow.com/student-camp-2022', permanent: true },
       { source: '/secretchurch', destination: 'https://longhollow.com/secret-church', permanent: true },
