56938c499618afd6d93e229bdf33d622e2cfa2df
Caleb Panza <45076058+calebpanza@users.noreply.github.com>
2022-03-11 17:00:38 -0500

    HTML Rendering Text Styles (#432)
    * Patch for inset lists
    
    * Font Spacing updates
    
    * Check In
    
    * Update HtmlRenderer.styles.js
    
    Co-authored-by: asleepingpanda <45076058+asleepingpanda@users.noreply.github.com>

--- ui-kit/HtmlRenderer/HtmlRenderer.styles.js
+++ ui-kit/HtmlRenderer/HtmlRenderer.styles.js
@@ -6,7 +6,18 @@ import { primaryHover } from 'ui-kit/Button/Button.styles';
 const HtmlRenderer = styled.div`
   > ul,
   > ol {
-      margin-left: ${themeGet('space.base')};
+    margin-top: ${themeGet('space.base')};
+    margin-bottom: ${themeGet('space.base')};
+  }
+
+  ul,
+  ol {
+    padding-left: ${themeGet('space.base')};
+
+    li {
+      margin-top: ${themeGet('space.xs')};
+      margin-bottom: ${themeGet('space.xs')};
+    }
   }
   
   a {
@@ -19,21 +30,46 @@ const HtmlRenderer = styled.div`
     }
   }
 
+  p {
+    margin-bottom: 0.75rem;
+  }
+
+  // H1-H3 will be considered "title" tags
   h1,
   h2,
-  h3,
+  h3 {
+    margin-top: 1.65rem;
+    margin-bottom: 0.4rem;
+  }
+
+  // H4-H6 will be considered "subtitle" tags
   h4,
   h5,
-  h6,
-  p {
-    &:not(:first-child) {
-      margin-top: ${themeGet('space.base')};
-      margin-bottom: ${themeGet('space.base')};
-    }
+  h6 {
+    margin-bottom: 0.75rem;
   }
 
   img {
     border-radius: ${themeGet('radii.base')};
+    margin-top: ${themeGet('space.base')};
+    margin-bottom: ${themeGet('space.base')};
+  }
+
+  blockquote {
+    font-family: ${themeGet('fonts.serif')};
+    font-style: italic;
+    font-weight: normal;
+    font-size: 1.15rem;
+    line-height: 2.15rem;
+
+    color: ${themeGet('colors.neutrals.700')};
+    text-align: center;
+
+    border-top: 1px solid ${themeGet('colors.neutrals.300')};
+    border-bottom: 1px solid ${themeGet('colors.neutrals.300')};
+
+    padding: ${themeGet('space.l')};
+    margin: ${themeGet('space.l')} ${themeGet('space.base')};
   }
 
   ${system};
