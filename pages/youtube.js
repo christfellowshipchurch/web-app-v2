8790522e0dbe9397a3cc1a111d75be9009258801
asleepingpanda <45076058+asleepingpanda@users.noreply.github.com>
2022-03-14 11:14:23 -0400

    Create youtube.js

---
+++ pages/youtube.js
@@ -0,0 +1,34 @@
+/**
+ * youtube.js
+ * 
+ * Author: Caleb Panza
+ * Created: Mar 14, 2022
+ * 
+ * description
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ContentLayout, Layout } from 'components';
+import { Box } from 'ui-kit'
+
+const youtube = ({ }) => {
+    return <Layout>
+        <ContentLayout
+            renderA={() => <iframe 
+                width="100%"
+                height="500px"
+                src="https://www.youtube.com/embed/bumfVOTKPmI" 
+                title="YouTube Analytic Test" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen />}
+            renderContentB={() => <Box as="h1">YouTube Analytic Test</Box>}
+        />
+    </Layout>
+};
+
+youtube.propTypes = {}
+youtube.defaultProps = {}
+
+export default youtube;
