1a06722f3ad96ae1513603f6168440b8d8f11fa6
dzwood <46049974+dzwood@users.noreply.github.com>
2022-07-05 16:26:25 -0400

    Added Error Catching for /profile

--- components/UserProfile/UserProfile.js
+++ components/UserProfile/UserProfile.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import { UserProfileProvider } from 'providers';
 import { Box, Cell, Loader } from 'ui-kit';
 import UserProfileContent from './UserProfileContent';
 import UserProfileHeader from './UserProfileHeader';
-
+import { useRouter } from 'next/router';
 function UserProfile(props = {}) {
+  const router = useRouter();
+
   if (props.loading) return <Loader text="Loading your profile" />;
+
+  useEffect(() => {
+    if (!props?.loading && props?.currentPerson?.length < 1) {
+      console.error(
+        'Unable to load current person, user is most likely not logged in.'
+      );
+      return router.push('/error');
+    }
+  }, []);
+
   return (
     <UserProfileProvider currentUser={props.currentPerson}>
       <Box mb="l" textAlign="center">
