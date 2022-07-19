8978f28d53bc91c74a735aa19aed45c54c8d9fce
bchamberlain <92399176+baychamberlain@users.noreply.github.com>
2022-07-19 10:30:41 -0500

    sermon notes

--- next.config.js
+++ next.config.js
@@ -264,7 +264,7 @@ module.exports = {
       { source: '/events/lh-kids-baptism-classes', destination: '/kids-baptism-classes', permanent: true },
       { source: '/events/supporting-those-who-struggle-event', destination: '/supporting-those-who-struggle-event', permanent: true },
       { source: '/events/light-up-the-pike', destination: 'https://lightupthepike.com', permanent: true },
-      { source: '/notes', destination: 'http://bible.com/events/48921495', permanent: true }
+      { source: '/notes', destination: 'http://bible.com/events/48925128', permanent: true }
     ];
   },
 };
