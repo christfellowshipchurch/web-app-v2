01763e51fd86fed10b0adae36bbd19df6c73f5c7
dzwood <46049974+dzwood@users.noreply.github.com>
2022-10-07 12:07:00 -0400

    Added Rock Content Block to Westlake Landing Page (#473)
    * Added useContentBlockFeature hook
    
    Decided to add this for custom pages when we need to pull in a specific ContentBlock from Rock.
    
    * Added Rock Content Block to Westlake Page

--- pages/locations/westlake/index.js
+++ pages/locations/westlake/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FAQ, HeroLanding, Layout } from 'components';
 import { Box, CardCarousel, ContentBlock, Image } from 'ui-kit';
+import { useContentBlockFeature } from 'hooks';
 import faqData from 'components/FAQ/faqData';
 
 const Westlake = props => {
+  //Grabs Upcoming Events Content Block from Rock
+  const { block } = useContentBlockFeature({
+    variables: {
+      id: 'ContentBlockFeature:d0d7407920381ab5b3b4d32cd65762c6f75ed64f8616c1ca5784f8c79bb069b7',
+    },
+  });
+
   return (
     <Layout
       contentMaxWidth={'100vw'}
@@ -71,11 +79,14 @@ const Westlake = props => {
       <Box px="base" py="l">
         <Box mt={{ _: '-0.5rem', md: 'base' }} mx="auto" maxWidth={900}>
           <ContentBlock
-            title={`Upcoming Events`}
-            htmlContent={`</br> Learn more about Christ Fellowship Westlake—including meeting the Campus Pastors—at an upcoming interest meeting near you! </br> </br> Sunday, October 2 | Christ Fellowship <a href="palm-beach-gardens">Palm Beach Gardens</a> in the Luncheon - Cafe Multipurpose Room. </br> </br>`}
-            image="/westlake/campus_pastors.png"
-            imageRatio="4by3"
-            contentLayout="left"
+            title={block?.title}
+            subtitle={block?.subtitle}
+            actions={block?.actions}
+            contentLayout={block?.orientation}
+            htmlContent={block?.htmlContent}
+            image={block?.coverImage?.sources[0]?.uri}
+            imageRatio={block?.imageRatio}
+            videos={block?.videos}
           />
         </Box>
       </Box>
