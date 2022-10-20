9f7c7b2754236aaad10cbc2bed3b8700ce3deee6
Lis Alfonso <46769629+codingwithlis@users.noreply.github.com>
2022-10-20 15:09:17 -0400

    updated copy - by Hannah

--- components/LocationBlockFeature/LocationBlockFeature.js
+++ components/LocationBlockFeature/LocationBlockFeature.js
@@ -31,9 +31,13 @@ const LocationBlockFeature = props => {
     //replaces default content with Trinity specific content
     content[0].subtitle = 'Babies to Elementary – Every Sunday at 10AM';
     content[0].htmlContent =
-      'A place designed just for kids where they’ll learn to love Jesus, love others, and love life.';
+      'Every Sunday while you attend service, Christ Fellowship Kids is a place designed just for your kids where they’ll learn to love Jesus, love others, and love life.';
     content[1].subtitle = 'Middle School & High School – Available Regionally';
+    content[1].htmlContent =
+      'CFStudents is available at just down the road at our Palm Beach Gardens campus location. Your student will find a strong community, learn what it means to follow Jesus, and discover their God-given identity and purpose.';
     content[2].subtitle = 'College & Young Adults – Available Regionally';
+    content[2].htmlContent =
+      'Christ Fellowship Young Adults meet down the road at our Palm Beach Gardens campus location. Join a community of college students, young adults, and young professionals.';
   }
 
   return (
