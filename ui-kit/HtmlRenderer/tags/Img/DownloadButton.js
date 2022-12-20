ba059d9d067a4473729c62e2c9afa591f463f580
Lis Alfonso <46769629+codingwithlis@users.noreply.github.com>
2022-12-20 11:21:52 -0500

    Update DownloadButton.js

--- ui-kit/HtmlRenderer/tags/Img/DownloadButton.js
+++ ui-kit/HtmlRenderer/tags/Img/DownloadButton.js
@@ -4,20 +4,17 @@ import PropTypes from 'prop-types';
 import { Box, Icon } from 'ui-kit';
 import Styled from './Img.styles';
 
-/**
- * const handleShareButton = () => {
-    // Check if navigator.share is supported by the browser
-    if (navigator.share) {
-      console.log("Congrats! Your browser supports Web Share API");
-      navigator
-        .share({
-          url: #enter-url
-        })
-    } else {
-      // return default button
-    }
-  };
- */
+const handleShareButton = () => {
+  // Check if navigator.share is supported by the browser
+  if (navigator.share) {
+    console.log('Congrats! Your browser supports Web Share API');
+    navigator.share({
+      url: '',
+    });
+  } else {
+    // return default button
+  }
+};
 
 const DownloadButton = (props = {}) => {
   const [status, setStatus] = useState('IDLE'); // IDLE | ACTIVE | DONE
@@ -33,16 +30,17 @@ const DownloadButton = (props = {}) => {
   return (
     <Styled.DownloadButton
       status={status}
-      onClick={() => {
-        setStatus('DONE');
-      }}
+      // onClick={() => {
+      //   setStatus('DONE');
+      // }}
+      onClick={handleShareButton}
       onMouseEnter={() => setStatus('ACTIVE')}
       onMouseLeave={() => {
         if (status === 'DONE') return;
         setStatus('IDLE');
       }}
-      href={`/api/image?src=${props?.source}`}
-      download="ChristFellowshipChurch"
+      // href={`/api/image?src=${props?.source}`}
+      // download="ChristFellowshipChurch"
     >
       <Icon
         name={status === 'DONE' ? 'check' : 'download'}
@@ -72,3 +70,38 @@ DownloadButton.defaultProps = {
 };
 
 export default DownloadButton;
+
+{
+  /* <div className="App">
+<h1>React Web Share API</h1>
+<button
+  onClick={handleShareButton}
+  className="share-button"
+  type="button"
+  title="Share this article"
+>
+  <svg>
+    <use href="#share-icon" />
+  </svg>
+  <span>Share</span>
+</button>
+<svg>
+  <defs>
+    <symbol
+      id="share-icon"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-share"
+    >
+      <path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8" />
+      <polyline points="16 6 12 2 8 6" />
+      <line x1="12" y1="2" x2="12" y2="15" />
+    </symbol>
+  </defs>
+</svg>
+</div> */
+}
