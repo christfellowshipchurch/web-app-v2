aea8983caab773d7ec7f0221a67021c2120901c2
ericmurrell <ericmurrell@gmail.com>
2023-03-02 10:11:31 -0600

    Force a stupid reboot

--- next.config.js
+++ next.config.js
@@ -31,7 +31,7 @@ module.exports = {
         source: '/app-link/:slug',
         destination: '/:slug',
         permanent: false,
-      }, // cache test
+      }, // reboot this stupid thing
       { source: '/feedback', destination: 'https://longhollow.wufoo.com/forms/vision-plan-strategy-feedback/', permanent: true },
       { source: '/identity', destination: 'https://longhollow.com/resources-about-sexuality-gender-and-identity', permanent: true },
       { source: '/retreat', destination: 'https://www.eventcreate.com/e/longhollowstaffretreat', permanent: true },
