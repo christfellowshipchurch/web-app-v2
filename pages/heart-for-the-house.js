a933d804a85f0fc867e5a30a9b2a40b09234824c
dzwood <46049974+dzwood@users.noreply.github.com>
2023-04-13 11:45:51 -0400

    [CFDP-2517] Added Custom Testimonial Section

--- pages/heart-for-the-house.js
+++ pages/heart-for-the-house.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 
-import { Layout } from 'components';
+import { Layout, Testimonials } from 'components';
 import { useCurrentBreakpoint } from 'hooks';
-import { CoverImage } from 'ui-kit';
+import { Box, CoverImage, ThemeMixin } from 'ui-kit';
 
 function HeartForTheHouse(props = {}) {
   const [imageSize, setImageSize] = useState('');
@@ -22,22 +22,59 @@ function HeartForTheHouse(props = {}) {
 
   return (
     <Layout>
-      <CoverImage
-        type="hero-glass"
-        src={`/heart-for-house/banners/header${imageSize}.jpg`}
-      />
-      <CoverImage
-        type="hero-glass"
-        src={`/heart-for-house/banners/year-review${
-          imageSize === '-large' ? '' : imageSize
-        }.jpg`}
-      />
-      <CoverImage
-        type="hero-glass"
-        src={`/heart-for-house/banners/vision${
-          imageSize === '-large' ? '' : imageSize
-        }.jpg`}
-      />
+      <ThemeMixin
+        theme={{
+          colors: {
+            primary: '#E63E51',
+            secondary: '#133156',
+          },
+        }}
+      >
+        <CoverImage
+          type="hero-glass"
+          src={`/heart-for-house/banners/header${imageSize}.jpg`}
+        />
+        <CoverImage
+          type="hero-glass"
+          src={`/heart-for-house/banners/year-review${
+            imageSize === '-large' ? '' : imageSize
+          }.jpg`}
+        />
+        <CoverImage
+          type="hero-glass"
+          src={`/heart-for-house/banners/vision${
+            imageSize === '-large' ? '' : imageSize
+          }.jpg`}
+        />
+        <Box py="xxl" bg="secondary">
+          <Testimonials
+            py="l"
+            maxWidth={1200}
+            m="auto"
+            title='<span style="color:#FFFFFF">This is our </span><span style="color:#E63E51">heart.</span>'
+            testimonies={[
+              {
+                name: '<p style="color:#E63E51;margin-bottom: 6px;">Alex and Melissa</p><p style="color:#E63E51;font-weight:normal"><i>Christ Fellowship Palm Beach Gardens</i></p>',
+                description:
+                  '<b>We can’t give enough in comparison to what God has given our family through Christ Fellowship.</b>',
+                image: '/heart-for-house/heart-logo-box.png',
+              },
+              {
+                name: '<p style="color:#E63E51;margin-bottom: 6px;">Kyle and Michelle</p><p style="color:#E63E51;font-weight:normal"><i>Christ Fellowship Young Adults</i></p>',
+                description:
+                  '<b>Each time I walk through the doors of Christ Fellowship on a Sunday, I immediately feel like I am at home.</b>',
+                image: '/heart-for-house/heart-logo-box.png',
+              },
+              {
+                name: '<p style="color:#E63E51;margin-bottom: 6px;">Kandice, 6 years old</p><p style="color:#E63E51;font-weight:normal"><i>Christ Fellowship Kids</i></p>',
+                description:
+                  '<b>I added a second piggy bank to outdo myself in giving because that’s what I’ve seen my mommy do!</b>',
+                image: '/heart-for-house/heart-logo-box.png',
+              },
+            ]}
+          />
+        </Box>
+      </ThemeMixin>
     </Layout>
   );
 }
