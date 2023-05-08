5e8ae271217c1543dbc3c5f5ab03a132012c9746
Lis Alfonso <46769629+codingwithlis@users.noreply.github.com>
2023-05-08 12:53:39 -0400

    creating new joyce meyer redirect

--- next.config.js
+++ next.config.js
@@ -102,6 +102,11 @@ module.exports = {
         destination: '/events/amazing',
         permanent: true,
       },
+      {
+        source:
+          '/items/god-lives-in-you-joyce-meyer-9638eed9752c44acffee9861ef2db161',
+        destination: '/messages/god-lives-in-you-joyce-meyer',
+      },
       // TODO: Uncomment these lines to hide Group Finder.
       // NOTE: We can't get `config/flags` in this file.
       // {
