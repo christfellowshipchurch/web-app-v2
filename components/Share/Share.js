f1ffc465bad67422bd318f0c697244cc6f706e06
natashasthilaire <natashasthilaire@ufl.edu>
2023-06-05 11:19:32 -0400

    updated button

--- components/Share/Share.js
+++ components/Share/Share.js
@@ -25,7 +25,8 @@ const Share = props => {
       renderTrigger={({ toggle }) => (
         <Button id={uniqueId('share-')} onClick={toggle} display="flex">
           <Icon name="share" mr="xs" />
-          { breakpoints.isXLarge || breakpoints.isLarge ? props.shareTitle : '' }
+          { breakpoints.isXLarge || 
+          breakpoints.isLarge ? props.shareTitle : '' }
         </Button>
       )}
       display="inline-flex"
