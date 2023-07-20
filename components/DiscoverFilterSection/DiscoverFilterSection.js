0c69e965ffa0c83ba5cdcda6f649049fdeaf8ee7
natashasthilaire <natashasthilaire@ufl.edu>
2023-07-20 10:54:33 -0400

    fixed view all

--- components/DiscoverFilterSection/DiscoverFilterSection.js
+++ components/DiscoverFilterSection/DiscoverFilterSection.js
@@ -26,7 +26,7 @@ const DiscoverFilterSection = ({ contentId, title, filter }) => {
   }).contentItems.length;
 
   const handleSeeMore = event => {
-    const [id] = contentId.split(':');
+    const [type,id] = contentId.split(':');
 
     event.preventDefault();
     router.push(`/discover/${slugify(title)}?id=${slugify(id)}&c=${filter}`);
