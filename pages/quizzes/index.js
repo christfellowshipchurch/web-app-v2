116400180473423dd32215ba6e6103889f4a848b
natashasthilaire <natashasthilaire@ufl.edu>
2023-08-01 09:46:52 -0400

    typeform page

---
+++ pages/quizzes/index.js
@@ -0,0 +1,11 @@
+
+import { Widget } from '@typeform/embed-react'
+
+export default function Quiz() {
+    return (
+        <div>
+         <Widget id="jJ7aDEPM" style={{ width: '100%', height:'800px'}} className="my-form" />
+      </div>
+    )
+}
+
