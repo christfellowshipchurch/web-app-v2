93bbebe1ff6a7873ea11ab50a803075cf3ddfdea
mhowell73 <112579538+mhowell73@users.noreply.github.com>
2023-08-29 21:21:15 -0500

    Update /girls-weekend

--- next.config.js
+++ next.config.js
@@ -76,7 +76,7 @@ module.exports = {
       { source: '/legacypark', destination: '/legacy-park-is-now-open', permanent: true },
       { source: '/park', destination: '/legacy-park-is-now-open', permanent: true },
       { source: '/food', destination: 'https://longhollow.com/foodforfamilies2022', permanent: true },
-      { source: '/girls-weekend', destination: 'https://longhollow.com/girls-weekend-2021', permanent: true },
+      { source: '/girls-weekend', destination: 'https://longhollow.com/girlsweekend-2023', permanent: true },
       { source: '/guys-weekend', destination: 'https://longhollow.com/guys-weekend-2021', permanent: true },
       { source: '/vote', destination: 'https://rock.longhollow.com/page/1267', permanent: true },
       { source: '/fallfamily', destination: '/fall-family-fun-day-2021', permanent: true },
