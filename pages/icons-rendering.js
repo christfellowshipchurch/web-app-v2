2aea59b93a143173ec43ab96c1dc537b088fc5af
Lis Alfonso <46769629+codingwithlis@users.noreply.github.com>
2023-09-27 10:08:43 -0400

    removed console.log

--- pages/icons-rendering.js
+++ pages/icons-rendering.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Layout, DisplayAllIcons } from 'components';
 import { Box } from 'ui-kit';
 
-console.log('shows');
-
 export default function IconsRendering() {
   return (
     <Layout>
