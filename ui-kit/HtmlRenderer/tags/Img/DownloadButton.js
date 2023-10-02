50e6486b4155f034e639efd9fd07dcd06b087f8d
leoalfonso14 <Leandroalfonso14@gmail.com>
2023-10-02 11:40:25 -0400

    Final Tests

--- ui-kit/HtmlRenderer/tags/Img/DownloadButton.js
+++ ui-kit/HtmlRenderer/tags/Img/DownloadButton.js
@@ -3,22 +3,22 @@ import PropTypes from 'prop-types';
 
 import { Box, Icon } from 'ui-kit';
 import Styled from './Img.styles';
-
-const handleShareButton = async shareData => {
-  const blob = await (await fetch(shareData)).blob();
-  const file = new File([blob], shareData.image, { type: blob.type });
-
-  try {
-    await navigator.share(shareData);
-    console.log('success');
-  } catch (err) {
-    console.log('share failed');
-  }
-};
+import { useCurrentBreakpoint } from 'hooks';
 
 const DownloadButton = (props = {}) => {
   const [status, setStatus] = useState('IDLE'); // IDLE | ACTIVE | DONE
+  const currentBreakpoint = useCurrentBreakpoint();
 
+  const handleShareButton = async shareData => {
+    if (currentBreakpoint.isSmall || currentBreakpoint.isMedium) {
+      try {
+        await navigator.share(shareData);
+        console.log('success');
+      } catch (err) {
+        console.log('share failed');
+      }
+    }
+  };
   useEffect(() => {
     if (status === 'DONE') {
       setTimeout(() => {
@@ -27,29 +27,24 @@ const DownloadButton = (props = {}) => {
     }
   }, [status]);
 
-  // const file = new File([blob], image, { type: 'image/jpeg' });
-
   return (
     <Styled.DownloadButton
       status={status}
-      // onClick={() => {
-      //   setStatus('DONE');
-      // }}
-      onClick={() =>
+      onClick={() => {
+        setStatus('DONE');
         handleShareButton({
           title: 'CF Images',
           text: 'Share this image!',
           image: `/api/image?src=${props?.source}`,
-          // files: [file],
-        })
-      }
+        });
+      }}
       onMouseEnter={() => setStatus('ACTIVE')}
       onMouseLeave={() => {
         if (status === 'DONE') return;
         setStatus('IDLE');
       }}
-      // href={`/api/image?src=${props?.source}`}
-      // download="ChristFellowshipChurch"
+      href={`/api/image?src=${props?.source}`}
+      download="ChristFellowshipChurch"
     >
       <Icon
         name={status === 'DONE' ? 'check' : 'download'}
@@ -79,38 +74,3 @@ DownloadButton.defaultProps = {
 };
 
 export default DownloadButton;
-
-{
-  /* <div className="App">
-<h1>React Web Share API</h1>
-<button
-  onClick={handleShareButton}
-  className="share-button"
-  type="button"
-  title="Share this article"
->
-  <svg>
-    <use href="#share-icon" />
-  </svg>
-  <span>Share</span>
-</button>
-<svg>
-  <defs>
-    <symbol
-      id="share-icon"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      class="feather feather-share"
-    >
-      <path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8" />
-      <polyline points="16 6 12 2 8 6" />
-      <line x1="12" y1="2" x2="12" y2="15" />
-    </symbol>
-  </defs>
-</svg>
-</div> */
-}
