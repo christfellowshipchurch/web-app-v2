a94968e0fab30384bb9089d2e66708721595eb52
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2023-10-11 14:58:34 -0400

    Group resources ordering (#621)
    * Resource Sorting
    
    * sorted resources
    
    * Sorting in group page

--- components/GroupSingle/GroupResources.js
+++ components/GroupSingle/GroupResources.js
@@ -14,34 +14,48 @@ export default function GroupResources(props = {}) {
   // typically derive a URL from the `relatedNode` regardless of
   // whether it's an `OPEN_URL` or `READ_CONTENT` etc action.
 
+  // Sort resources by title
+  const sortedResources = [...props.resources].sort((a, b) => {
+    const titleA = a.title || a.relatedNode?.title;
+    const titleB = b.title || b.relatedNode?.title;
+
+    if (titleA < titleB) {
+      return -1;
+    }
+
+    if (titleA > titleB) {
+      return 1;
+    }
+
+    return 0;
+  });
+
   return (
     <List>
-      {props.resources
-        .filter(resource => resource.title || resource.relatedNode?.title)
-        .map(resource => (
-          <Box key={resource.relatedNode?.id} as="li" display="flex">
-            <CustomLink
-              key={resource.relatedNode?.id}
-              href={getUrlFromRelatedNode(resource.relatedNode)}
-              textDecoration="none"
-              target="_blank"
-            >
-              <Box display="flex" alignItems="center">
-                <Image
-                  maxWidth="50px"
-                  source={
-                    resource.relatedNode?.coverImage?.sources[0]?.uri ||
-                    '/link.png'
-                  }
-                  mr="base"
-                />
-                <Box as="h4">
-                  {resource.title || resource.relatedNode?.title}
-                </Box>
+      {sortedResources.map(sortedResource => (
+        <Box key={sortedResource.relatedNode?.id} as="li" display="flex">
+          <CustomLink
+            key={sortedResource.relatedNode?.id}
+            href={getUrlFromRelatedNode(sortedResource.relatedNode)}
+            textDecoration="none"
+            target="_blank"
+          >
+            <Box display="flex" alignItems="center">
+              <Image
+                maxWidth="50px"
+                source={
+                  sortedResource.relatedNode?.coverImage?.sources[0]?.uri ||
+                  '/link.png'
+                }
+                mr="base"
+              />
+              <Box as="h4">
+                {sortedResource.title || sortedResource.relatedNode?.title}
               </Box>
-            </CustomLink>
-          </Box>
-        ))}
+            </Box>
+          </CustomLink>
+        </Box>
+      ))}
     </List>
   );
 }
