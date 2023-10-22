1a87f4ac38e5e1d6845d24f46d832343f9f9acc1
mhowell73 <112579538+mhowell73@users.noreply.github.com>
2023-10-22 09:35:31 -0500

    Update /Hang URL

--- next.config.js
+++ next.config.js
@@ -34,6 +34,7 @@ module.exports = {
       }, // reboot this stupid thing
       { source: '/family', destination: 'https://nsb2r6cpbt4.typeform.com/to/rv4waLbv', permanent: true },
       { source: '/fall', destination: '/fallatlh', permanent: true },
+      { source: '/hang', destination: '/momandmehang-nov2023', permanent: true },
       { source: '/onboarding', destination: 'https://rock.longhollow.com/page/827', permanent: true },
       { source: '/bettermangroups', destination: 'https://my.longhollow.com/Registration/bettermanstudy-oct2023', permanent: true },
       { source: '/hangout', destination: '/lhkidshomeschoolwheelsandreels', permanent: true },
