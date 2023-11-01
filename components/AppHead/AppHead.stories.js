56d7a7cb88ae62021f4daf31810db064c16c7d75
leoalfonso14 <Leandroalfonso14@gmail.com>
2023-11-01 14:55:47 -0400

    Added more stories
    Collection Preview
    CommunityActionSection
    CommunityLeaderActions
    CommunityList - work in progress

--- components/AppHead/AppHead.stories.js
+++
@@ -1,9 +0,0 @@
-import AppHead from './AppHead';
-
-export default {
-  component: AppHead,
-  tags: ['autodocs'],
-  argTypes: {},
-};
-
-export const Default = {};
---
+++ components/CommunityList/CommunityList.stories.js
@@ -0,0 +1,14 @@
+import CommunityList from './CommunityList';
+
+export default {
+  component: CommunityList,
+  tags: ['autodocs'],
+  argTypes: {},
+};
+
+export const Default = {
+  args: {
+    data: {},
+    loading: true,
+  },
+};
