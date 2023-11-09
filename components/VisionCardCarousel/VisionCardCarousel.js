950092b30f01293504351236429ad99c86f04b97
dzwood <46049974+dzwood@users.noreply.github.com>
2023-11-09 12:42:02 -0500

    Fixed Vision Card Carousel

--- components/VisionCardCarousel/VisionCardCarousel.js
+++ components/VisionCardCarousel/VisionCardCarousel.js
@@ -1,5 +1,8 @@
-import React from 'react';
-import { Box, Card, CardCarousel, Icon } from 'ui-kit';
+import React, { useRef } from 'react';
+import { useDraggable } from 'react-use-draggable-scroll';
+
+import { Box, HtmlRenderer, Icon } from 'ui-kit';
+import { StyledScrollBox, StyledCard } from './VisionCardCarousel.styles';
 
 import visionCards from './visionCards';
 
@@ -8,6 +11,11 @@ import visionCards from './visionCards';
  */
 
 function VisionCardCarousel() {
+  const ref = useRef(); // We will use React useRef hook to reference the wrapping div:
+  const { events } = useDraggable(ref, {
+    applyRubberBandEffect: true,
+  }); // Now we pass the reference to the useDraggable hook:
+
   return (
     <Box
       id="vision"
@@ -21,21 +29,43 @@ function VisionCardCarousel() {
         mx="auto"
         src="/christ-birthday-offering/vision-logo.png"
       />
-      <Box as="h4" textAlign="center" maxWidth={600} my="base" mx="auto">
+      <Box
+        as="h4"
+        textAlign="center"
+        maxWidth={600}
+        my="base"
+        mx="auto"
+        px="base"
+      >
         Every dollar you give to Christ Birthday Offering will make a difference
         toward continuing our impact both locally and globally
       </Box>
 
-      <CardCarousel cardsDisplayed={3.5}>
+      {/* Card Scroll */}
+      <StyledScrollBox {...events} ref={ref}>
+        {/* Blank box to offset scroll */}
+        <Box
+          minWidth={{ _: 20, md: 40 }}
+          ml="base"
+          py="xl"
+          textAlign="center"
+        />
+        {/* Vision Cards */}
         {visionCards.map(({ icon, text }, index) => (
-          <Card height="100%" ml="base" py="xl" textAlign="center">
-            <Icon name={icon} size={60} />
-            <Box mt="base" mx="l">
-              {text}h
+          <StyledCard
+            minWidth={{ _: 250, md: 300 }}
+            mx="s"
+            textAlign="center"
+            my="l"
+            py="l"
+          >
+            <Icon name={icon} size={{ _: 50, md: 60 }} />
+            <Box mt="l" mx="base">
+              <HtmlRenderer htmlContent={text} />
             </Box>
-          </Card>
+          </StyledCard>
         ))}
-      </CardCarousel>
+      </StyledScrollBox>
     </Box>
   );
 }
