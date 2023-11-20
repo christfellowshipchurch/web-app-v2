1ab5256df8153e84df3e5fd26b3061397b16e8d5
leoalfonso14 <Leandroalfonso14@gmail.com>
2023-11-20 12:58:30 -0500

    Added CTA

--- pages/2023-christ-birthday-offering/index.js
+++ pages/2023-christ-birthday-offering/index.js
@@ -71,9 +71,12 @@ const ChristBirthdayOffering = () => {
           {/* Header Video */}
           <VideoHeader
             backgroundVideo={{
-              desktop: '/external-landing/external-bg-vid.mp4',
+              desktop: '/christ-birthday-offering/cbo-header-vid.mp4',
             }}
             logoOverlay={'christ-birthday-offering/banners/header.png'}
+            backgroundImage="/christ-birthday-offering/cbo-header-frame-1.png"
+            backgroundPosition="center"
+            backgroundSize="cover"
           />
 
           {/* Give a Different Gift - Content Block */}
@@ -153,10 +156,20 @@ const ChristBirthdayOffering = () => {
                 titleColor="#353535"
                 options={{
                   variables: {
-                    id: 'VerticalCardListFeature:f6b30a38eb547f1f75676ce0557a7e05bed66de665564bd0c5e37f0692676795e12dca024e16de4c6d7782ed0288eb10570bc45a488bee66c93ee7b8735beb7675d6a9ab1a060d39594fa3a2468a99fb13ffe67e9661e7cccdbf404007bc650e51ef2d61925e9a10c9569d1891c8854171e84f094fc6165d87b4f4dadeab61733ccfb6a3c7eeaf8d85017b5ad501acdbfdda5f8036fc3708b0c33464c5875d3420e0d58a296064a2093339d331556792e1d12f6ea001a3c7be6f5ca631a105ba207391542715f900de42cca2dd7bfde1252a1f1c4f6373277f060126b84ecf12ecfd49a86d705f170b13dc39c8089d42946e6b1f5e795e83caa6bdaed029cc13',
+                    id: 'VerticalCardListFeature:f6b30a38eb547f1f75676ce0557a7e05bed66de665564bd0c5e37f0692676795e12dca024e16de4c6d7782ed0288eb10570bc45a488bee66c93ee7b8735beb7675d6a9ab1a060d39594fa3a2468a99fb13ffe67e9661e7cccdbf404007bc650e51ef2d61925e9a10c9569d1891c8854171e84f094fc6165d87b4f4dadeab61733ccfb6a3c7eeaf8d85017b5ad501acdb93ee9c0c776d30d487a3f09da22c8335897f16cf807a814826c09e05dbd656ba',
                   },
                 }}
               />
+              <Button
+                as="a"
+                mt="l"
+                display="flex"
+                mx="auto"
+                width="fit-content"
+                href="https://issuu.com/christfellowshipchurch/docs/events_2023_cbo_print_handouts_8.5x11_spanish_web_?fr=xKAE9_zU1NQ"
+              >
+                GIVE YOUR OFFERING
+              </Button>
             </Box>
           </Box>
 
