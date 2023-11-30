6235438b1a7670b3535c44004dae8fb119cffca9
mhowell73 <112579538+mhowell73@users.noreply.github.com>
2023-11-30 08:51:10 -0600

    Update /first responder URL

--- next.config.js
+++ next.config.js
@@ -34,7 +34,7 @@ module.exports = {
       }, // reboot this stupid thing
       { source: '/family', destination: 'https://nsb2r6cpbt4.typeform.com/to/rv4waLbv', permanent: true },
       { source: '/hope', destination: '/hopefortheholidays-november2023', permanent: true },
-      { source: '/firstrespondermeals', destination: '/first-responder-meals-help-us-care-for-our-first-responders-on-thanksgiving', permanent: true },
+      { source: '/firstrespondermeals', destination: '/first-responder-meals-help-us-care-for-our-first-responders-on-christmas', permanent: true },
       { source: '/fall', destination: '/fallatlh', permanent: true },
       { source: '/good', destination: '/get-involved', permanent: true },
       { source: '/wild', destination: 'https://my.longhollow.com/page/1759?ProjectId=140rpr189K&LocationId=x5yjnKGyeL&ScheduleId=n8YWP1X0Bv', permanent: true },
