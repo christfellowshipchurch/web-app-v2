dfe761dd4eeedc99392836976047f974ec8ea4bf
mhowell73 <112579538+mhowell73@users.noreply.github.com>
2023-12-12 15:07:43 -0600

    Update Created Redirect for URL /Wednesdays

--- next.config.js
+++ next.config.js
@@ -35,6 +35,7 @@ module.exports = {
       { source: '/family', destination: 'https://nsb2r6cpbt4.typeform.com/to/rv4waLbv', permanent: true },
       { source: '/relief', destination: 'https://longhollow.com/community-relief-at-long-hollow', permanent: true },
       { source: '/hope', destination: '/hopefortheholidays-november2023', permanent: true },
+      { source: '/wednesdays', destination: '/wednesdaysatlhwinter-2024', permanent: true },
       { source: '/firstrespondermeals', destination: '/first-responder-meals-help-us-care-for-our-first-responders-on-christmas', permanent: true },
       { source: '/fall', destination: '/fallatlh', permanent: true },
       { source: '/good', destination: '/get-involved', permanent: true },
