358fd8bf273e268181edc9dd7c21d98367aa0ba5
mhowell73 <112579538+mhowell73@users.noreply.github.com>
2023-12-12 20:02:20 -0600

    Update /discover-your-calling URL

--- next.config.js
+++ next.config.js
@@ -245,6 +245,7 @@ module.exports = {
       { source: '/jesus', destination: '/about/who-is-jesus', permanent: true },
       { source: '/gospel', destination: '/about/who-is-jesus', permanent: true },
       { source: '/discover', destination: '//wednesdaysatlhwinter-2024', permanent: true },
+      { source: '/discover-you-calling', destination: '//wednesdaysatlhwinter-2024', permanent: true },
       { source: '/the-good-news', destination: '/about/who-is-jesus', permanent: true },
       { source: '/what-we-believe', destination: '/about/beliefs', permanent: true },
       { source: '/should-i-be-baptized', destination: '/next-steps/baptism', permanent: true },
