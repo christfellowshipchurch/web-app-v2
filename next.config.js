76c3d3dbf5ca3b5027ec42c0cff6f759d05a05d2
mhowell73 <112579538+mhowell73@users.noreply.github.com>
2024-01-04 09:29:43 -0600

    Update /21days URL

--- next.config.js
+++ next.config.js
@@ -79,7 +79,7 @@ module.exports = {
       { source: '/mvmnt', destination: 'https://longhollow.com/mvmnt-2022', permanent: true },
       { source: '/game', destination: 'https://longhollow.com/family-game-night', permanent: true },
       { source: '/studentmissions', destination: 'https://longhollow.com/studentspringbreakmissiontrips2024', permanent: true },
-      { source: '/21days', destination: 'https://longhollow.com/start-the-new-year-with-21-days-of-devotion', permanent: true },
+      { source: '/21days', destination: 'https://longhollow.com/21daysdevotion', permanent: true },
       { source: '/share', destination: 'https://my.longhollow.com/Registration/share', permanent: true },
       { source: '/report', destination: 'https://annualreport.longhollow.com', permanent: true },
       { source: '/annualreport', destination: 'https://annualreport.longhollow.com', permanent: true },
