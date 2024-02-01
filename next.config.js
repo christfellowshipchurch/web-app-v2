98a46e0d07345ff7de81b0ed7636087ad5a74ec4
mhowell73 <112579538+mhowell73@users.noreply.github.com>
2024-02-01 09:25:21 -0600

    Update /partner URL

--- next.config.js
+++ next.config.js
@@ -51,7 +51,7 @@ module.exports = {
       { source: '/foster', destination: '/fosterandadoptivefamilynight', permanent: true },
       { source: '/knot', destination: '/theknotmarriageconference', permanent: true },
       { source: '/wednesday', destination: '/wednesdaysatlhwinter-2024', permanent: true },
-      { source: '/partner', destination: '/get-strong-ministries', permanent: true },
+      { source: '/partner', destination: '/thepowersfamily', permanent: true },
       { source: '/seven', destination: 'https://signature.rezdy.com/582215/pastor-robby-gallaty-11-day-journey-to-the-seven-churches-october-4-14-2024', permanent: true },
       { source: '/sevenchurches', destination: 'https://signature.rezdy.com/582215/pastor-robby-gallaty-11-day-journey-to-the-seven-churches-october-4-14-2024', permanent: true },
       { source: '/turkey', destination: 'https://signature.rezdy.com/582215/pastor-robby-gallaty-11-day-journey-to-the-seven-churches-october-4-14-2024', permanent: true },
