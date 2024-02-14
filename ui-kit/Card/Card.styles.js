a15f642fc20ed1eee7d1860bdead8fa08ce78b85
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2024-02-14 11:06:58 -0500

    Added mobile with as prop

--- ui-kit/Card/Card.styles.js
+++ ui-kit/Card/Card.styles.js
@@ -70,7 +70,7 @@ const Card = styled.div`
   overflow: ${props => (props.scaleCoverImage ? 'hidden' : 'initial')};
 
   @media screen and (max-width: ${themeGet('breakpoints.md')}) {
-    width: 80vw;
+    width: ${props => (props.mobileWidth ? `${props.mobileWidth}` : 'auto')};
   }
 
   ${link}
