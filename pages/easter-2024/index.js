9efe101c6712e45050ca6bf2b8a9bc7c4001db8a
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2024-02-16 10:30:09 -0500

    Created Page

---
+++ pages/easter-2024/index.js
@@ -0,0 +1,17 @@
+import { Layout } from 'components';
+import { Box } from 'ui-kit';
+
+const EasterPage = () => {
+  return (
+    <Layout>
+      {/* Header Section */}
+      <Box> Easter 2024</Box>
+      {/* Video Section Section */}
+      {/* Kids Programming Section */}
+      {/* Serve Section */}
+      {/* FAQ Section Section */}
+    </Layout>
+  );
+};
+
+export default EasterPage;
