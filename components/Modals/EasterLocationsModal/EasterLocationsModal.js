8a1cd42f8dba6cd88515a4039e3dd5b5a22fe964
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2024-02-23 15:49:57 -0500

    Easter Modal Update

--- components/Modals/EasterLocationsModal/EasterLocationsModal.js
+++ components/Modals/EasterLocationsModal/EasterLocationsModal.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import { Box, Modal } from 'ui-kit';
+import { Box, Button, Divider, Modal } from 'ui-kit';
 function EasterLocationsModal(props = {}) {
   return (
-    <Modal width="80vw" {...props}>
+    <Modal width={{ _: '90vw', md: '80vw' }} {...props}>
       <Box color="black">
         <Box display="flex" alignItems="flex-end">
           <Box as="h1" fontSize={54} mb={0}>
@@ -25,7 +25,7 @@ function EasterLocationsModal(props = {}) {
         >
           {props?.address}5343 Northlake Blvd, Palm Beach Gardens, FL 33418
         </Box>
-        <Box display="flex">
+        <Box display="flex" flexDirection={{ _: 'column', md: 'row' }}>
           <Box color="#353535" mt="l">
             <Box>
               <Box as="h3" fontSize={32} textDecoration="underline">
@@ -35,21 +35,59 @@ function EasterLocationsModal(props = {}) {
                 Easter
               </Box>
 
-              <Box mt="base">
+              <Box mt="base" fontSize={12}>
                 <Box color="#818181" fontStyle="italic">
                   *Spanish Translation
                 </Box>
                 <Box>
                   Special programming for babies – 6th grade.{' '}
                   <Box as="a" color="#3B7DD9" href="/christ-fellowship-kids">
-                    Learn More {`>`}
+                    Learn More
                   </Box>
                 </Box>
               </Box>
             </Box>
           </Box>
-          <Box mt="l">
-            <Box>Don’t Miss Service</Box>
+          <Box mt="l" borderRadius="8px" border="1px solid black">
+            <Box mx="base" my="base">
+              <Box
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
+              >
+                <Box fontSize={18} fontWeight="bold">
+                  Don’t Miss Service
+                </Box>
+                <Button
+                  mt="s"
+                  bg="#3B7DD9"
+                  border="1px solid #000"
+                  borderRadius="50px"
+                >
+                  Add to Calendar
+                </Button>
+
+                <Divider mt="base" />
+              </Box>
+              <Box
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
+              >
+                <Box fontSize={18} fontWeight="bold" mt="base">
+                  Invite A Friend
+                </Box>
+                <Button
+                  mt="s"
+                  bg="#FFEC7F"
+                  color="black"
+                  border="1px solid #000"
+                  borderRadius="50px"
+                >
+                  Send a Text Message
+                </Button>
+              </Box>
+            </Box>
           </Box>
         </Box>
       </Box>
