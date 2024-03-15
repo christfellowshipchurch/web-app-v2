d0a9591122cc52caa7cd304fbbedce6fe7f43d4f
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2024-03-15 11:19:46 -0400

    give-page-layout

--- pages/give/index.js
+++ pages/give/index.js
@@ -1,51 +1,23 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-import { initializeApollo } from 'lib/apolloClient';
-import { GET_FEATURE_FEED } from 'hooks/useFeatureFeed';
-import { FeatureFeedProvider } from 'providers';
-import { Layout, FeatureFeed } from 'components';
-import { Cell, utils } from 'ui-kit';
-import { useAnalytics } from 'providers/AnalyticsProvider';
+import { Layout } from 'components';
 
 export default function Give(props) {
-  const analytics = useAnalytics();
-  const options = {
-    variables: {
-      pathname: 'give',
-    },
-  };
-
-  //Segment Page Tracking
-  useEffect(() => {
-    analytics.page();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
     <Layout title="Give">
-      <Cell
-        as="main"
-        maxWidth={utils.rem('1100px')}
-        px="base"
-        py={{ _: 'base', lg: 'l' }}
-      >
-        <FeatureFeedProvider Component={FeatureFeed} options={options} />
-      </Cell>
+      {/* Hero */}
+
+      {/* Pushpay */}
+
+      {/* Ways to Give */}
+
+      {/* Wistia Carouse */}
+
+      {/* Rock Content #1 */}
+
+      {/* Rock Content #2 */}
+
+      {/* FAQ */}
     </Layout>
   );
-}
-
-export async function getServerSideProps() {
-  const apolloClient = initializeApollo();
-
-  await apolloClient.query({
-    query: GET_FEATURE_FEED,
-    variables: { pathname: 'give' },
-  });
-
-  return {
-    props: {
-      initialApolloState: apolloClient.cache.extract(),
-    },
-  };
 }
