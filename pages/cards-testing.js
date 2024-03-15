8360d4ed5d36c3b5726c861d963380311bae9e38
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2024-03-15 10:01:09 -0400

    Updates Cards Testing Page

--- pages/cards-testing.js
+++ pages/cards-testing.js
@@ -1,8 +1,16 @@
-import { Layout } from 'components';
+import { CardGridFeature, Layout } from 'components';
+import { useDiscoverFilterCategoriesPreview } from 'hooks';
 import React from 'react';
 import { Box, DefaultCard } from 'ui-kit';
 
 const CardsTesting = props => {
+  const testingId = 'UniversalContentItem:ddf0d380759e8404fb6b70aa941c06f7';
+  let contentItems = [];
+  const { contentItems: queriedContent } = useDiscoverFilterCategoriesPreview({
+    variables: { id: testingId, first: 3 },
+  });
+  contentItems = queriedContent;
+
   return (
     <Layout>
       <Box>
@@ -18,6 +26,11 @@ const CardsTesting = props => {
             width={{ _: '90vw', md: 900 }}
           />
         </Box>
+        <CardGridFeature
+          data={{ cards: contentItems }}
+          horizontalScroll={true}
+          customCardSize="HIGHLIGHT_MEDIUM"
+        />
       </Box>
     </Layout>
   );
