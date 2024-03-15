740bb2c4d9491706e5b86e40cd84b741e8299b04
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2024-03-15 12:12:29 -0400

    FAQ added

--- pages/give/index.js
+++ pages/give/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
-import { Layout } from 'components';
+import { FAQ, Layout } from 'components';
+import { Box } from 'ui-kit';
+import faqData from 'components/FAQ/faqData';
 
 export default function Give(props) {
   return (
@@ -18,6 +20,22 @@ export default function Give(props) {
       {/* Rock Content #2 */}
 
       {/* FAQ */}
+      {/* FAQ Section Section */}
+      <Box mx="auto" maxWidth={1200} px="base">
+        <FAQ
+          customTheme={{ secondary: '#39383A' }}
+          py="xl"
+          mt="2.5rem !important"
+          data={faqData('Give')}
+          otherData={{
+            title: 'FAQ',
+            question: 'Have additional questions?',
+            description:
+              '<div>Call <span style="font-weight: bold;">561-776-3380</span> to speak to someone on our team that would love to help you.</div>',
+            descriptionOverride: true,
+          }}
+        />
+      </Box>
     </Layout>
   );
 }
