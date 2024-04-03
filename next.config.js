538afa5b5e270289a40df39ca1673343046dbc8f
mhowell73 <112579538+mhowell73@users.noreply.github.com>
2024-04-03 13:01:45 -0500

    Update /summerserve URL

--- next.config.js
+++ next.config.js
@@ -72,7 +72,7 @@ module.exports = {
       { source: '/lightupthepike', destination: 'https://lightupthepike.com/', permanent: true },
       { source: '/cruisein', destination: 'https://longhollow.wufoo.com/forms/pyyg54d1egc91m/', permanent: true },
       { source: '/young-adults-weekly-gathering', destination: '/tuesday-night-gathering', permanent: true },
-      { source: '/summerserve', destination: '/next-steps/volunteer', permanent: true },
+      { source: '/summerserve', destination: '/volunteer', permanent: true },
       { source: '/loveourcity', destination: '/serve-your-community-during-love-our-city', permanent: true },
       { source: '/compassion', destination: '/our-partnership-with-compassion-international', permanent: true },
       { source: '/summerjam', destination: '/bring-your-family-to-summer-jam-with-lh-kids', permanent: true },
