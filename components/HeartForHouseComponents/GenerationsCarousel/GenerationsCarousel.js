781b7736bfb6f41c730db4cf749adf520eaa97c6
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2024-04-15 10:06:57 -0400

    carousel testing

--- components/HeartForHouseComponents/GenerationsCarousel/GenerationsCarousel.js
+++ components/HeartForHouseComponents/GenerationsCarousel/GenerationsCarousel.js
@@ -2,7 +2,7 @@
  * This component is a carousel that displays the 40th Anniversary photos for the H4H 2024 campaign. We will be using Embla Carousel to create this component.
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import useEmblaCarousel from 'embla-carousel-react';
 import AutoScroll from 'embla-carousel-auto-scroll';
 
@@ -14,7 +14,24 @@ function GenerationsCarousel({ id }) {
   const [emblaRef] = useEmblaCarousel({ loop: true }, [
     AutoScroll({ stopOnInteraction: false, speed: 1 }),
   ]);
-
+  const [overlay, setOverlay] = useState(false);
+  const [overlayValue, setOverlayValue] = useState(null);
+  const imageHover = [
+    { image: 1, date: 'January 1999', description: 'UN Trip' },
+    { image: 2, date: 'February 1999', description: 'UN Trip' },
+    { image: 3, date: 'March 1999', description: 'UN Trip' },
+    { image: 4, date: 'April 1999', description: 'UN Trip' },
+    { image: 5, date: 'May 1999', description: 'UN Trip' },
+    { image: 6, date: 'June 1999', description: 'UN Trip' },
+    { image: 7, date: 'July 1999', description: 'UN Trip' },
+    { image: 8, date: 'August 1999', description: 'UN Trip' },
+    { image: 9, date: 'September 1999', description: 'UN Trip' },
+    { image: 10, date: 'October 1999', description: 'UN Trip' },
+    { image: 11, date: 'November 1999', description: 'UN Trip' },
+    { image: 12, date: 'December 1999', description: 'UN Trip' },
+    { image: 13, date: 'January 2000', description: 'UN Trip' },
+    { image: 14, date: 'February 2000', description: 'UN Trip' },
+  ];
   const numberOfSlides = 14;
 
   return (
@@ -47,14 +64,29 @@ function GenerationsCarousel({ id }) {
                       xl: '0 0 28%',
                     }}
                     minWidth={0}
+                    position="relative"
                   >
                     <ImageHover
+                      key={index}
                       borderRadius="0px"
                       maxHeight={{ _: 200, md: 250, lg: 300 }}
+                      onMouseEnter={e => {
+                        setOverlay(true);
+                        setOverlayValue(index);
+                      }}
+                      onMouseLeave={e => {
+                        setOverlay(false);
+                      }}
                       source={`/heart-for-house/carousel/generations-photos/${
                         index + 1
                       }.jpg`}
                     />
+                    {overlay && overlayValue === index && (
+                      <Box position="absolute" top={230} ml="s" color="white">
+                        <Box mt="base">{imageHover[index].description}</Box>
+                        <Box>{imageHover[index].date}</Box>
+                      </Box>
+                    )}
                   </Box>
                 );
               })}
