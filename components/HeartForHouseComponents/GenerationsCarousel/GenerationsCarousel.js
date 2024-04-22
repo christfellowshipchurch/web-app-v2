521eb9b1753be5ebbcc536311dadf03b77c03814
leoalfonso14 <94265294+leoalfonso14@users.noreply.github.com>
2024-04-22 09:59:08 -0400

    FAQ & Photo Carousel Copy Update

--- components/HeartForHouseComponents/GenerationsCarousel/GenerationsCarousel.js
+++ components/HeartForHouseComponents/GenerationsCarousel/GenerationsCarousel.js
@@ -27,22 +27,84 @@ function GenerationsCarousel({ id }) {
   const imageHover = [
     {
       image: 1,
-      date: 'January 1985',
-      description: 'Original Home Group in the Kitchen',
+      date: '1984',
+      description: 'Early church gathering at the Mullins’ home',
     },
-    { image: 2, date: 'February 1999', description: 'UN Trip' },
-    { image: 3, date: 'March 1999', description: 'UN Trip' },
-    { image: 4, date: 'April 1999', description: 'UN Trip' },
-    { image: 5, date: 'May 1999', description: 'UN Trip' },
-    { image: 6, date: 'June 1999', description: 'UN Trip' },
-    { image: 7, date: 'July 1999', description: 'UN Trip' },
-    { image: 8, date: 'June 1998', description: 'UN Trip' },
-    { image: 9, date: 'September 1999', description: 'Baby Dedication' },
-    { image: 10, date: 'October 1999', description: 'UN Trip' },
-    { image: 11, date: 'November 1999', description: 'UN Trip' },
-    { image: 12, date: 'December 1999', description: 'UN Trip' },
-    { image: 13, date: 'January 2000', description: 'UN Trip' },
-    { image: 14, date: 'February 2000', description: 'UN Trip' },
+    {
+      image: 2,
+      date: '1984',
+      description: 'The beginnings of Christ Fellowship Church',
+    },
+    {
+      image: 3,
+      date: '1991',
+      description:
+        'Palm Beach Gardens South—Christ Fellowship’s first permanent location',
+    },
+    {
+      image: 4,
+      date: '1999',
+      description:
+        'Palm Beach Gardens North—Christ Fellowship moves to 40-acre property after explosive growth',
+    },
+    {
+      image: 5,
+      date: '1984',
+      description:
+        'After outgrowing the living room, Christ Fellowship starts meeting at Grove Park Elementary School',
+    },
+    {
+      image: 6,
+      date: '1987',
+      description: 'Pastors Todd and Julie start the first youth group',
+    },
+    {
+      image: 7,
+      date: '1996',
+      description: 'The beginning of Christ Fellowship Missions',
+    },
+    {
+      image: 8,
+      date: '1998',
+      description:
+        'One of the child dedications in the early days of Christ Fellowship',
+    },
+    {
+      image: 9,
+      date: '1984',
+      description:
+        "The first-ever baptism, which took place in a church member's pool",
+    },
+    {
+      image: 10,
+      date: '2000',
+      description:
+        'Palm Beach Gardens North opens the doors to its first services',
+    },
+    {
+      image: 11,
+      date: '1984',
+      description:
+        'Pastor Tom Mullins (R) stands with the first church sign outside Grove Park Elementary School',
+    },
+    {
+      image: 12,
+      date: '2006',
+      description:
+        'Christ Fellowship purchases its first multi-site location—a former Target store in Royal Palm Beach',
+    },
+    {
+      image: 13,
+      date: '1984',
+      description:
+        'Pastors Tom & Donna Mullins, Christ Fellowship’s founding pastors',
+    },
+    {
+      image: 14,
+      date: '1992',
+      description:
+        'Pastor Tom and Pastor Todd stand in front of the construction for Palm Beach Gardens South',
+    },
   ];
 
   const numberOfSlides = 14;
@@ -90,7 +152,13 @@ function GenerationsCarousel({ id }) {
                         }.jpg`}
                       />
                       <HoverOverlay height={{ _: 200, md: 250, lg: 300 }}>
-                        <Box fontSize="h3" fontWeight="bold">
+                        <Box
+                          fontSize="h3"
+                          fontWeight="bold"
+                          textAlign="center"
+                          mx="base"
+                          style={{ textWrap: 'pretty' }}
+                        >
                           {imageHover[index].description}
                         </Box>
                         <Box>{imageHover[index].date}</Box>
