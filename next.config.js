e35d687baec004906523fec6a89a46ae4b1eaffb
mhowell73 <112579538+mhowell73@users.noreply.github.com>
2024-05-15 11:12:41 -0500

    Update Sermon Notes

--- next.config.js
+++ next.config.js
@@ -306,7 +306,7 @@ module.exports = {
       { source: '/events/light-up-the-pike', destination: 'https://lightupthepike.com', permanent: true },
       { source: '/groupserve', destination: 'https://my.longhollow.com/groupserve', permanent: true },
       { source: '/vendor', destination: 'https://longhollow.wufoo.com/forms/phu7t8c0lslify/', permanent: true },
-      { source: '/notes', destination: 'http://bible.com/events/49256092', permanent: true }
+      { source: '/notes', destination: 'http://bible.com/events/49259562', permanent: true }
     ];
   },
 };
