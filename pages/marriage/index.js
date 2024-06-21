73f27de877b2e6f536bb5095afb36c4ef064c1be
Daniel Wood <Dzwood327@gmail.com>
2024-06-21 16:49:04 -0400

    Added Page

---
+++ pages/marriage/index.js
@@ -0,0 +1,42 @@
+import Head from 'next/head';
+import parseHtml from 'html-react-parser';
+import { Footer, Header } from 'components';
+import { Box } from 'ui-kit';
+
+export default function TimelinePage(props) {
+  return (
+    <Box bg="black" overflow="none">
+      <Head>{parseHtml(props.headContent)}</Head>
+      <Header />
+      <div dangerouslySetInnerHTML={{ __html: props.bodyContent }} />
+      <Footer />
+    </Box>
+  );
+}
+
+export async function getStaticProps(ctx) {
+  // Import modules in here that aren't needed in the component
+  const cheerio = await import(`cheerio`);
+  const axios = (await import(`axios`)).default;
+
+  // Fetch HTML
+  let res = await axios(
+    'https://cfdp-marketing-site.webflow.io/married-people'
+  ).catch(err => {
+    console.error(err);
+  });
+  const html = res.data;
+
+  // Parse HTML with Cheerio
+  const $ = cheerio.load(html);
+  const bodyContent = $(`body`).html();
+  const headContent = $(`head`).html();
+
+  // Send HTML to component via props
+  return {
+    props: {
+      bodyContent,
+      headContent,
+    },
+  };
+}
