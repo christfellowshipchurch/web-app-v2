d21b766510dd8722dcc0a328243150c6bdf103da
Daniel Wood <Dzwood327@gmail.com>
2024-06-21 12:48:30 -0400

    [CFDP-3097] Removed Download Button
    We have decided to remove the download button from images, realizing that it is unnecessary and we can't find a better solution for sharing graphics.

--- ui-kit/HtmlRenderer/tags/Img/Img.js
+++ ui-kit/HtmlRenderer/tags/Img/Img.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import isEmpty from 'lodash/isEmpty'
+import isEmpty from 'lodash/isEmpty';
 
 import Styled from './Img.styles';
 import { Box, Image } from 'ui-kit';
-import DownloadButton from './DownloadButton';
+// import DownloadButton from './DownloadButton';
 
 const Img = (props = {}) => {
   return (
     <Styled>
-      <DownloadButton source={props?.source} />
+      {/* Removing this for the time being */}
+      {/* <DownloadButton source={props?.source} /> */}
       <Image {...props} />
 
-      {!isEmpty(props?.alt) && <Box 
-        as="span"
-        fontStyle="italic"
-        color="neutrals.600"
-        mt="s"
-      >
-        {props?.alt}
-        </Box>}
+      {!isEmpty(props?.alt) && (
+        <Box as="span" fontStyle="italic" color="neutrals.600" mt="s">
+          {props?.alt}
+        </Box>
+      )}
     </Styled>
   );
 };
