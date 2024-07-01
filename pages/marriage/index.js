c8c32cabe8adc625225090df12119f1ed2e14356
Daniel Wood <Dzwood327@gmail.com>
2024-07-01 09:58:39 -0400

    Moved fetch to clientside

--- pages/marriage/index.js
+++ pages/marriage/index.js
@@ -1,12 +1,52 @@
 import Head from 'next/head';
 import parseHtml from 'html-react-parser';
-import { Footer, Header } from 'components';
-import { Box } from 'ui-kit';
+import axios from 'axios';
+import { load } from 'cheerio';
+import { Footer, Header, NotFound } from 'components';
+import { Box, Loader } from 'ui-kit';
 import { useAnalytics } from 'providers/AnalyticsProvider';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
-export default function TimelinePage(props) {
+export default function MarriagePage(props) {
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(true);
   const analytics = useAnalytics();
+
+  useEffect(() => {
+    const fetchWebflowPage = async () => {
+      // Fetch HTML
+      let res = await axios(
+        'https://cfdp-marketing-site.webflow.io/married-people'
+      ).catch(err => {
+        console.error(err);
+      });
+
+      if (res) {
+        const html = res?.data;
+
+        // Parse HTML with Cheerio
+        const $ = load(html);
+        const bodyContent = $(`body`).html();
+        const headContent = $(`head`).html();
+
+        // Send HTML to component via props
+        return {
+          props: {
+            bodyContent,
+            headContent,
+          },
+        };
+      } else {
+        return null;
+      }
+    };
+
+    fetchWebflowPage().then(data => {
+      setData(data);
+      setLoading(false);
+    });
+  }, []);
+
   useEffect(() => {
     analytics.page({
       contentCategory: 'Information',
@@ -15,39 +55,45 @@ export default function TimelinePage(props) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  if (!data) return <NotFound />;
+
   return (
     <Box bg="black" overflow="none">
-      <Head>{parseHtml(props.headContent)}</Head>
+      <Head key="marriage">{parseHtml(data?.props?.headContent)}</Head>
       <Header />
-      <div dangerouslySetInnerHTML={{ __html: props.bodyContent }} />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div dangerouslySetInnerHTML={{ __html: data?.props?.bodyContent }} />
+      )}
       <Footer />
     </Box>
   );
 }
 
-export async function getStaticProps(ctx) {
-  // Import modules in here that aren't needed in the component
-  const cheerio = await import(`cheerio`);
-  const axios = (await import(`axios`)).default;
+// export async function getStaticProps(ctx) {
+//   // Import modules in here that aren't needed in the component
+//   const cheerio = await import(`cheerio`);
+//   const axios = (await import(`axios`)).default;
 
-  // Fetch HTML
-  let res = await axios(
-    'https://cfdp-marketing-site.webflow.io/married-people'
-  ).catch(err => {
-    console.error(err);
-  });
-  const html = res.data;
+//   // Fetch HTML
+//   let res = await axios(
+//     'https://cfdp-marketing-site.webflow.io/married-people'
+//   ).catch(err => {
+//     console.error(err);
+//   });
+//   const html = res.data;
 
-  // Parse HTML with Cheerio
-  const $ = cheerio.load(html);
-  const bodyContent = $(`body`).html();
-  const headContent = $(`head`).html();
+//   // Parse HTML with Cheerio
+//   const $ = cheerio.load(html);
+//   const bodyContent = $(`body`).html();
+//   const headContent = $(`head`).html();
 
-  // Send HTML to component via props
-  return {
-    props: {
-      bodyContent,
-      headContent,
-    },
-  };
-}
+//   // Send HTML to component via props
+//   return {
+//     props: {
+//       bodyContent,
+//       headContent,
+//     },
+//   };
+// }
