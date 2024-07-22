71c90a2389464f0e2e86e76d445d69e6cbc040db
Daniel Wood <46049974+dzwood@users.noreply.github.com>
2024-07-22 10:45:06 -0400

    Added One Life Page and Updated Document

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
@@ -13,8 +13,18 @@ export default class MyDocument extends Document {
         });
 
       const initialProps = await Document.getInitialProps(ctx);
+
+      // Note : This is a hack to add Webflow data attributes to the HTML tag
+      let webflowDataAttributes = '';
+      if (ctx.pathname === '/one-life') {
+        webflowDataAttributes =
+          'data-wf-page="668fe801f1a28d6ad35222da" data-wf-site="66749aec3acbf8aa6ef9d378"';
+      }
+
       return {
         ...initialProps,
+        // Overriding HTML tag so we can add Webflow data attributes
+        html: `<html lang="en" ${webflowDataAttributes}>${initialProps?.html}</html>`,
         styles: (
           <>
             {initialProps.styles}
@@ -26,4 +36,21 @@ export default class MyDocument extends Document {
       sheet.seal();
     }
   }
+
+  // Use this render method if we run into too many issues with the above method for the Webflow One Life page
+  // render() {
+  //   return (
+  //     <Html
+  //       lang="en"
+  //       data-wf-page="668fe801f1a28d6ad35222da"
+  //       data-wf-site="66749aec3acbf8aa6ef9d378"
+  //     >
+  //       <Head />
+  //       <body>
+  //         <Main />
+  //         <NextScript />
+  //       </body>
+  //     </Html>
+  //   );
+  // }
 }
