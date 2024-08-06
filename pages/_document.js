ae998b5e45bc031eaccd7021178a06ea62268dd9
Daniel Wood <46049974+dzwood@users.noreply.github.com>
2024-08-06 16:02:28 -0400

    One Life Loading Bug Fix (#708)
    * Hardcoded Webflow Page/Site Id
    
    * Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -15,16 +15,16 @@ export default class MyDocument extends Document {
       const initialProps = await Document.getInitialProps(ctx);
 
       // Note : This is a hack to add Webflow data attributes to the HTML tag
-      let webflowDataAttributes = '';
-      if (ctx.pathname === '/one-life') {
-        webflowDataAttributes =
-          'data-wf-page="668fe801f1a28d6ad35222da" data-wf-site="66749aec3acbf8aa6ef9d378"';
-      }
+      // let webflowDataAttributes = '';
+      // if (ctx.pathname === '/one-life') {
+      //   webflowDataAttributes =
+      //     'data-wf-page="668fe801f1a28d6ad35222da" data-wf-site="66749aec3acbf8aa6ef9d378"';
+      // }
 
       return {
         ...initialProps,
         // Overriding HTML tag so we can add Webflow data attributes
-        html: `<html lang="en" ${webflowDataAttributes}>${initialProps?.html}</html>`,
+        //html: `<html lang="en" ${webflowDataAttributes}>${initialProps?.html}</html>`,
         styles: (
           <>
             {initialProps.styles}
@@ -38,19 +38,21 @@ export default class MyDocument extends Document {
   }
 
   // Use this render method if we run into too many issues with the above method for the Webflow One Life page
-  // render() {
-  //   return (
-  //     <Html
-  //       lang="en"
-  //       data-wf-page="668fe801f1a28d6ad35222da"
-  //       data-wf-site="66749aec3acbf8aa6ef9d378"
-  //     >
-  //       <Head />
-  //       <body>
-  //         <Main />
-  //         <NextScript />
-  //       </body>
-  //     </Html>
-  //   );
-  // }
+  render() {
+    return (
+      <Html
+        lang="en"
+        //hard coding the One Life page data attributes
+        // todo: find a better way to do this
+        data-wf-page="668fe801f1a28d6ad35222da"
+        data-wf-site="66749aec3acbf8aa6ef9d378"
+      >
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
