7b74af4cc128cd4e80d1cc3cf6684549445cd446
Daniel Wood <46049974+dzwood@users.noreply.github.com>
2024-08-07 09:53:40 -0400

    Removed Google Page Translation Listener
    We shouldn't need this code anymore because it exist in the Google Tag Manager container.

--- components/AppHead/AppHead.js
+++ components/AppHead/AppHead.js
@@ -136,66 +136,6 @@ function AppHead({ Component, pageProps }) {
         }}
       />
 
-      {/* Observe DOM mutations whether the <html> node was changed by Google Translate */}
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `(
-  function() {
-    if (window.MutationObserver) {
-      var mutationObserver = new MutationObserver(function(mutations) {
-        mutations.forEach(function (mutation) {
-          var oldElementClass = mutation.oldValue;
-          var currentElementClass = mutation.target.className;
-          if (oldElementClass.indexOf('translated-') === -1 && currentElementClass.indexOf('translated-') > -1) {
-            window.dataLayer.push({
-              'event' : 'pageTranslated',
-              'translationLanguage' : mutation.target.lang || document.getElementsByTagName('html')[0].getAttribute('xml:lang'),
-              'translationService' : 'on-page google translate'
-            })
-          }
-      })
-    })
-      
-      var htmlNode = document.querySelector('html');
-      mutationObserver.observe(htmlNode, {
-        attributes: true,
-        attributeOldValue: true,
-        attributeFilter: ['class']
-      })
-    }
-
-
-
-    // Let's also track pageviews when the page is translated directly from translate.google.com or bing.com/translator
-    // A function that can return individual query parameter
-    function getUrlParameter(name) {
-      name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-      var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-      var results = regex.exec(location.search);
-      return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-    }
-
-    // Check if the page is being translated directly from translate.google.com (viewed within the iframe)
-    if (window.location.href.indexOf('translate.googleusercontent.com') > -1 ) {
-      window.dataLayer.push({
-        'event' : 'pageTranslated',
-        'translationLanguage' : getUrlParameter('tl'),
-        'translationService' : 'google translate website'
-      })
-    }
-
-    // Check if the page is being translated directly from bing.com/translator (viewed within the iframe)
-    if (window.location.href.indexOf('translatoruser-int.com') > -1 ) {
-      window.dataLayer.push({
-        'event' : 'pageTranslated',
-        'translationLanguage' : getUrlParameter('to'),
-        'translationService' : 'bing translator website'
-      })
-    }
-    })();`,
-        }}
-      />
-
       {/* Global Site Code Pixel - Facebook Pixel */}
       {/* Facebook meta-tag for new iOS 14 website verification */}
       <meta
