9d1836094b17516e9da2af17f4dfa85f0831e17f
Daniel Wood <46049974+dzwood@users.noreply.github.com>
2024-09-12 14:50:27 -0400

    Added Next Steps Page ID

--- pages/_document.js
+++ pages/_document.js
@@ -14,17 +14,28 @@ export default class MyDocument extends Document {
 
       const initialProps = await Document.getInitialProps(ctx);
 
-      // Note : This is a hack to add Webflow data attributes to the HTML tag
-      // let webflowDataAttributes = '';
-      // if (ctx.pathname === '/one-life') {
-      //   webflowDataAttributes =
-      //     'data-wf-page="668fe801f1a28d6ad35222da" data-wf-site="66749aec3acbf8aa6ef9d378"';
-      // }
+      /**
+       *  Note : This is a hack to add Webflow data attributes to the HTML tag
+       *  In order for us to load animations and interactions from Webflow we need to pass the page ID to the HTML tag
+       */
+      let webflowDataAttributes = '';
+      switch (ctx.pathname) {
+        case '/one-life':
+          webflowDataAttributes = 'data-wf-page="668fe801f1a28d6ad35222da"';
+          break;
+        case '/next-steps':
+          webflowDataAttributes = 'data-wf-page="66d89a03d565e226f00e0083"';
+          break;
+        default:
+          // Default case if no matching path is found
+          webflowDataAttributes = '';
+          break;
+      }
 
       return {
         ...initialProps,
         // Overriding HTML tag so we can add Webflow data attributes
-        //html: `<html lang="en" ${webflowDataAttributes}>${initialProps?.html}</html>`,
+        html: `<html lang="en" ${webflowDataAttributes}>${initialProps?.html}</html>`,
         styles: (
           <>
             {initialProps.styles}
@@ -42,9 +53,7 @@ export default class MyDocument extends Document {
     return (
       <Html
         lang="en"
-        //hard coding the One Life page data attributes
-        // todo: find a better way to do this
-        data-wf-page="668fe801f1a28d6ad35222da"
+        // Webflow Site ID
         data-wf-site="66749aec3acbf8aa6ef9d378"
       >
         <Head />
