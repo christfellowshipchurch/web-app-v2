fa08f1882b36e6695e4ce0abafb244a135663ba9
mhowell73 <112579538+mhowell73@users.noreply.github.com>
2024-10-06 09:42:19 -0500

    Update /offering URL

--- next.config.js
+++ next.config.js
@@ -267,7 +267,7 @@ module.exports = {
       { source: '/replicate', destination: 'https://replicate.org/', permanent: true },
       { source: '/bylaws', destination: '/about/beliefs', permanent: true },
       { source: '/divorcecare', destination: '/divorce-care', permanent: true },
-      { source: '/offering', destination: '/easteroffering', permanent: true },
+      { source: '/offering', destination: '/fall-offering-2024', permanent: true },
       { source: '/ministryplan', destination: '/the-2024-ministry-plan', permanent: true },
       { source: '/griefshare', destination: '/grief-share', permanent: true },
       { source: '/food', destination: '/next-steps/get-involved-with-missions', permanent: true },
