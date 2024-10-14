070894e761d218e7d876dca9035994e352c151f2
Leandro Alfonso <94265294+leoalfonso14@users.noreply.github.com>
2024-10-14 11:29:33 -0400

    Added Metadata to Location Search (#722)

--- components/LocationSearch/LocationSearch.js
+++ components/LocationSearch/LocationSearch.js
@@ -97,7 +97,15 @@ const LocationSearch = () => {
   }
 
   return (
-    <Layout>
+    <Layout
+      title="Christ Fellowship Church Locations | Across South Florida and Online"
+      seoMetaTags={{
+        keywords:
+          'christ fellowship church locations, church locations, church locations near me, church, church near me, church in my area, churches, churches near me, churches in my area, christian church, christian church near me, non denominational church, non denominational church near me, church services, church services near me',
+        description:
+          'Here at Christ Fellowship Church, we want to help you live the life you were created for. Every Sunday, we have church services throughout South Florida where you can experience uplifting worship music, encouraging messages from our pastors, special programming for your family, and opportunities for you to find people to do life with all throughout the week—and it all starts here!',
+      }}
+    >
       <LocationHeader
         handleSubmit={handleSubmit}
         setAddress={setAddress}
