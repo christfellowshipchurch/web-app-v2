2045a53ca68170ef17fba6203919ea48c9478835
mhowell73 <112579538+mhowell73@users.noreply.github.com>
2024-12-11 09:12:17 -0600

    Create short URL /launchnight

--- next.config.js
+++ next.config.js
@@ -67,6 +67,7 @@ module.exports = {
       { source: '/mensnight', destination: 'https://longhollow.com/lh-mens-night-come-hang-with-us', permanent: true },
       { source: '/group-leader-resources', destination: '/groupleaderresources', permanent: true },
       { source: '/preview', destination: '/launch-night-winter-2025', permanent: true },
+      { source: '/launchnight', destination: '/launch-night-winter-2025', permanent: true },
       { source: '/mondaynightoutreach', destination: '/monday-night-outreach-provide-meals', permanent: true },
       { source: '/prison', destination: '/prison-ministry-volunteer-information-meeting', permanent: true },
       { source: '/events', destination: 'https://longhollow.com/search?category=Events&p=1', permanent: true },
