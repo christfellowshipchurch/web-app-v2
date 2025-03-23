418974f6dfd90e250cd12b5d650306fe4e15eeb1
ericmurrell <ericmurrell@gmail.com>
2025-03-23 07:17:04 -0500

    easter site

--- next.config.js
+++ next.config.js
@@ -324,7 +324,7 @@ module.exports = {
       { source: '/baby', destination: '/connect/kids', permanent: true },
       { source: '/local', destination: '/search?category=Service%20Opportunities&p=1', permanent: true },
       { source: '/seniors', destination: '/', permanent: true },
-      { source: '/easter', destination: '/easter2024', permanent: true },
+      { source: '/easter', destination: 'https://longholloweaster.com', permanent: true },
       { source: '/lhkids', destination: '/connect/kids', permanent: true },
       { source: '/christmas', destination: 'https://longhollowchristmas.com', permanent: true },
       { source: '/christmaseve', destination: '/christmas-eve-at-long-hollow', permanent: true },
