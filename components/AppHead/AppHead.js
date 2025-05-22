4cbced1eda93f59e581f15964a6afe3acdf7a659
Daniel Wood <46049974+dzwood@users.noreply.github.com>
2025-05-22 16:49:43 -0400

    Added Twitter Code Snippet (#741)

--- components/AppHead/AppHead.js
+++ components/AppHead/AppHead.js
@@ -266,6 +266,18 @@ function AppHead({ Component, pageProps }) {
           `,
         }}
       />
+
+      {/* Twitter conversion tracking base code */}
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+          },s.version='1.1',s.queue=[],u=t.createElementthumbs down,u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+      a=t.getElementsByTagNamethumbs down[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+      twq('config','${process.env.NEXT_PUBLIC_TWITTER_CODE}');
+      `,
+        }}
+      />
     </Head>
   );
 }
