1629d53a68a6e162d53fd99fec01edac1bc41bf2
mhowell73 <112579538+mhowell73@users.noreply.github.com>
2025-07-16 14:28:07 -0500

    Create short URL /movienight

--- next.config.js
+++ next.config.js
@@ -49,6 +49,7 @@ module.exports = {
       { source: '/startagroup', destination: 'https://longhollow.wufoo.com/forms/wyeo3uq1108qq7/', permanent: true },
       { source: '/job-opportunities', destination: 'https://recruiting.paylocity.com/recruiting/jobs/All/91a29838-d1d7-4bd7-bcd4-c5b7ce72873d/Long-Hollow-Church', permanent: true },
       { source: '/connect/current-job-opportunities', destination: 'https://recruiting.paylocity.com/recruiting/jobs/All/91a29838-d1d7-4bd7-bcd4-c5b7ce72873d/Long-Hollow-Church', permanent: true },
+      { source: '/movienight', destination: '/family-movie-night-light-of-the-world', permanent: true },
       { source: '/weekly', destination: '/long-hollow-weekly-main', permanent: true },
       { source: '/merch', destination: 'https://longhollow.store/', permanent: true },
       { source: '/prison', destination: '/jail-and-prison-ministry', permanent: true },
