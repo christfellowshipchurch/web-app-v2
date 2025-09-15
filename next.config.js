32f05d23e7d5deca2b2148ec5bbe3ab6ba9969ca
mhowell73 <112579538+mhowell73@users.noreply.github.com>
2025-09-15 10:03:35 -0500

    Update short link for Girls Weekend to /girlsweekend

--- next.config.js
+++ next.config.js
@@ -199,7 +199,7 @@ module.exports = {
       { source: '/legacypark', destination: '/legacy-park-is-now-open', permanent: true },
       { source: '/park', destination: '/legacy-park-is-now-open', permanent: true },
       { source: '/food', destination: '/food-for-families-filling-needs-in-our-neighborhoods', permanent: true },
-      { source: '/girls-weekend', destination: '/girls-weekend-2025-a-weekend-retreat-for-middle-school-and-high-school-girls', permanent: true },
+      { source: '/girlsweekend', destination: '/girls-weekend-2025-a-weekend-retreat-for-middle-school-and-high-school-girls', permanent: true },
       { source: '/guys-weekend', destination: '/guys-weekend-2025-a-weekend-retreat-for-middle-school-and-high-school-guys', permanent: true },
       { source: '/vote', destination: 'https://rock.longhollow.com/page/1267', permanent: true },
       { source: '/fallfamily', destination: '/fall-family-fun-day-2024', permanent: true },
