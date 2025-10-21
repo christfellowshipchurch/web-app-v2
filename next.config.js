e47afded1890b51d63bc82148f2dc84ba54068cf
mhowell73 <112579538+mhowell73@users.noreply.github.com>
2025-10-21 08:51:06 -0500

    Update short URL /gallatindatenight

--- next.config.js
+++ next.config.js
@@ -56,7 +56,7 @@ module.exports = {
       { source: '/mensnightgallatin', destination: '/gallatin-mens-night', permanent: true },
       { source: '/mensnight', destination: 'https://rock.longhollow.com/Content/Mens_Night_Information.pdf', permanent: true },
       { source: '/longhollowweekly', destination: '/long-hollow-weekly-main', permanent: true },
-      { source: '/gallatindate', destination: '/gallatin-date-night-a-songwriters-round', permanent: true },
+      { source: '/gallatindatenight', destination: '/gallatin-date-night-a-songwriters-round', permanent: true },
       { source: '/startagroup', destination: 'https://longhollow.wufoo.com/forms/wyeo3uq1108qq7/', permanent: true },
       { source: '/job-opportunities', destination: 'https://recruiting.paylocity.com/recruiting/jobs/All/91a29838-d1d7-4bd7-bcd4-c5b7ce72873d/Long-Hollow-Church', permanent: true },
       { source: '/connect/current-job-opportunities', destination: 'https://recruiting.paylocity.com/recruiting/jobs/All/91a29838-d1d7-4bd7-bcd4-c5b7ce72873d/Long-Hollow-Church', permanent: true },
