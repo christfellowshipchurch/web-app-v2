4fa372221795b1d941ef21a69b1ad141cabe7c59
mhowell73 <112579538+mhowell73@users.noreply.github.com>
2025-11-04 14:40:02 -0600

    Update /serveatchristmas

--- next.config.js
+++ next.config.js
@@ -100,7 +100,7 @@ module.exports = {
       { source: '/winterserve', destination: '/serve-at-christmas-2024-find-a-place-to-serve-at-long-hollow-during-the-holiday-season', permanent: true },
       { source: '/ice', destination: '/ice-rink-2024', permanent: true },
       { source: '/nightofworship', destination: '/long-hollow-worship-live-recording-and-worship-night', permanent: true },
-      { source: '/serveatchristmas', destination: '/serve-at-christmas-2024-find-a-place-to-serve-at-long-hollow-during-the-holiday-season', permanent: true },
+      { source: '/serveatchristmas', destination: '/serve-for-the-holidays-2025', permanent: true },
       { source: '/global', destination: '/global-missions', permanent: true },
       { source: '/missionsschedule', destination: '/missions-sunday-next-steps', permanent: true },
       { source: '/churchplanting', destination: '/church-planting', permanent: true },
